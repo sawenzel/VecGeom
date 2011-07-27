be0153614d483c932f9cc412e668a1d7aed9ba43
jean-marie.guyader <jean-marie.guyader@07d0e867-4f0b-4bfb-81c0-4958028a6c5b>
2011-07-27 16:17:22 +0000

    Automatic gneration of a determined number of nodes

--- bridges/TGeo/TestMultiUnion.C
+++ bridges/TGeo/TestMultiUnion.C
@@ -28,95 +28,45 @@ void TestMultiUnion()
 
    // Instance:
       // Creation of several nodes:
-   UBox *box1 = new UBox("UBox",80,80,80);
-   UBox *box2 = new UBox("UBox",100,100,100);
-   UBox *box3 = new UBox("UBox",100,100,100);
-   UBox *box4 = new UBox("UBox",20,20,20);
-   UBox *box5 = new UBox("UBox",50,50,50);
-   UBox *box6 = new UBox("UBox",60,60,60);
-   UBox *box7 = new UBox("UBox",30,60,60);
-   UBox *box8 = new UBox("UBox",90,10,10);
-   UBox *box9 = new UBox("UBox",40,40,40);             
-   
-   UTransform3D *transform1 = new UTransform3D(0,0,0,0,45,0);
-   UTransform3D *transform2 = new UTransform3D(300,300,300,0,0,0);
-   UTransform3D *transform3 = new UTransform3D(0,400,100,0,0,0);   
-   UTransform3D *transform4 = new UTransform3D(300,500,600,10,0,0);     
-   UTransform3D *transform5 = new UTransform3D(0,20,10,0,0,0);  
-   UTransform3D *transform6 = new UTransform3D(-200,25,100,0,90,0);   
-   UTransform3D *transform7 = new UTransform3D(0,-150,-300,70,90,30);  
-   UTransform3D *transform8 = new UTransform3D(600,0,0,0,45,0);            
-   UTransform3D *transform9 = new UTransform3D(0,0,0,0,20,63);                            
-                    
-      // Constructor:
-   UMultiUnion *multi_union = new UMultiUnion("multi_union");
-   multi_union->AddNode(box1,transform1);
-   multi_union->AddNode(box2,transform2);
-   multi_union->AddNode(box3,transform3);
-   multi_union->AddNode(box4,transform4);
-   multi_union->AddNode(box5,transform5);
-   multi_union->AddNode(box6,transform6);
-   multi_union->AddNode(box7,transform7);
-   multi_union->AddNode(box8,transform8);                
-   multi_union->AddNode(box9,transform9);                                     
-  
-   // Preparing RayTracing of the created geometry:
-      // Using bridge class "TGeoUShape":
-	TGeoCombiTrans *combi1 = new TGeoCombiTrans(0,0,0,new TGeoRotation("rot1",0,45,0));
-	TGeoCombiTrans *combi2 = new TGeoCombiTrans(300,300,300,new TGeoRotation("rot2",0,0,0));
-	TGeoCombiTrans *combi3 = new TGeoCombiTrans(0,400,100,new TGeoRotation("rot3",0,0,0));
-	TGeoCombiTrans *combi4 = new TGeoCombiTrans(300,500,600,new TGeoRotation("rot4",10,0,0));
-	TGeoCombiTrans *combi5 = new TGeoCombiTrans(0,20,10,new TGeoRotation("rot5",0,0,0));
-	TGeoCombiTrans *combi6 = new TGeoCombiTrans(-200,25,100,new TGeoRotation("rot6",0,90,0));  
-	TGeoCombiTrans *combi7 = new TGeoCombiTrans(0,-150,-300,new TGeoRotation("rot7",70,90,30));  
-	TGeoCombiTrans *combi8 = new TGeoCombiTrans(600,0,0,new TGeoRotation("rot8",0,45,0));             
-	TGeoCombiTrans *combi9 = new TGeoCombiTrans(0,0,0,new TGeoRotation("rot9",0,20,63));    
-      
-   TGeoUShape *Shape1 = new TGeoUShape("Shape1",box1);
+   UBox *box = new UBox("UBox",40,40,40);
+
+   TGeoUShape *Shape1 = new TGeoUShape("Shape1",box);
    TGeoVolume *Volume1 = new TGeoVolume("Volume1",Shape1);
    Volume1->SetLineColor(1);
-         
-   TGeoUShape *Shape2 = new TGeoUShape("Shape2",box2);
-   TGeoVolume *Volume2 = new TGeoVolume("Volume2",Shape2);
-   Volume2->SetLineColor(2);
    
-   TGeoUShape *Shape3 = new TGeoUShape("Shape3",box3);
-   TGeoVolume *Volume3 = new TGeoVolume("Volume3",Shape3);   
-   Volume3->SetLineColor(3);  
+      // Number of nodes to implement:
+   int numNodesImpl = 10;
+   int mIndex = 0, nIndex = 0, oIndex = 0;
+	int carBoxesX = 10;
+	int carBoxesY = 10;
+	int carBoxesZ = 10;   
+   UTransform3D* arrayTransformations[numNodesImpl];
+	TGeoCombiTrans* arrayCombiTrans[numNodesImpl];
   
-   TGeoUShape *Shape4 = new TGeoUShape("Shape4",box4);
-   TGeoVolume *Volume4 = new TGeoVolume("Volume4",Shape4);   
-   Volume4->SetLineColor(4);  
+     // Constructor:
+   UMultiUnion *multi_union = new UMultiUnion("multi_union");     
    
-   TGeoUShape *Shape5 = new TGeoUShape("Shape5",box5);
-   TGeoVolume *Volume5 = new TGeoVolume("Volume5",Shape5);   
-   Volume5->SetLineColor(5);  
-   
-   TGeoUShape *Shape6 = new TGeoUShape("Shape6",box6);
-   TGeoVolume *Volume6 = new TGeoVolume("Volume6",Shape6);   
-   Volume6->SetLineColor(6);    
-   
-   TGeoUShape *Shape7 = new TGeoUShape("Shape7",box7);
-   TGeoVolume *Volume7 = new TGeoVolume("Volume7",Shape7);   
-   Volume7->SetLineColor(7);   
-   
-   TGeoUShape *Shape8 = new TGeoUShape("Shape8",box8);
-   TGeoVolume *Volume8 = new TGeoVolume("Volume8",Shape8);   
-   Volume8->SetLineColor(8);   
-                                    
-   TGeoUShape *Shape9 = new TGeoUShape("Shape9",box9);
-   TGeoVolume *Volume9 = new TGeoVolume("Volume9",Shape9);   
-   Volume9->SetLineColor(9);   
-  
-   top->AddNode(Volume1,1,combi1);
-   top->AddNode(Volume2,2,combi2);        
-   top->AddNode(Volume3,3,combi3);        
-   top->AddNode(Volume4,4,combi4); 
-   top->AddNode(Volume5,5,combi5); 
-   top->AddNode(Volume6,6,combi6); 
-   top->AddNode(Volume7,7,combi7); 
-   top->AddNode(Volume8,8,combi8);            
-   top->AddNode(Volume9,9,combi9);    
+      // Transformation:
+   for(mIndex = 0 ; (mIndex < numNodesImpl) && (mIndex < carBoxesX*carBoxesY*carBoxesZ) ; mIndex++)
+   {
+      arrayTransformations[mIndex] = new UTransform3D(-1000+50+2*50*(mIndex%carBoxesX),-1000+50+2*50*nIndex,-1000+50+2*50*oIndex,0,0,0);
+      multi_union->AddNode(box,arrayTransformations[mIndex]);
+      
+      arrayCombiTrans[mIndex] = new TGeoCombiTrans(-1000+50+2*50*(mIndex%carBoxesX),-1000+50+2*50*nIndex,-1000+50+2*50*oIndex,new TGeoRotation("rot",0,0,0));      
+      top->AddNode(Volume1,mIndex+1,arrayCombiTrans[mIndex]);      
+      
+      // Preparing "Draw":
+      if((nIndex%carBoxesY)==(carBoxesY-1) && (mIndex%carBoxesX)==(carBoxesX-1))         
+      {
+         nIndex = -1;
+         oIndex++;
+      }      
+      
+		if((mIndex%carBoxesX)==(carBoxesX-1))
+		{
+			nIndex++;
+		}
+   }                                                                                                                                   
 
    geom->CloseGeometry();
 
@@ -136,7 +86,7 @@ void TestMultiUnion()
    double bmin[3], bmax[3];
    UVector3 point;
    multi_union->Extent(bmin, bmax);
-   int npoints = 100000;
+   int npoints = 10000000;
    TPolyMarker3D *pminside = new TPolyMarker3D();
    pminside->SetMarkerColor(kRed);
    TPolyMarker3D *pmoutside = new TPolyMarker3D();
