fa3a1428d77eff867ce4abfa05b7dbf1b970feef
tatiana.nikitina <tatiana.nikitina@07d0e867-4f0b-4bfb-81c0-4958028a6c5b>
2011-08-16 08:35:57 +0000

    Add Visualisation Methods needed by G4

--- include/Utypes.hh
+++ include/Utypes.hh
@@ -1,8 +1,10 @@
 #ifndef USOLIDS_Utypes
 #define USOLIDS_Utypes
 // Types defined by the unified solids library
+#include "UVector3.hh"
 #include <iostream>
 #include <string>
+#include <vector>
 
 typedef unsigned int UInt_t;
 struct UBBoxStruct {
@@ -25,6 +27,18 @@ struct UBuffer3DStruct {
    double   *fPnts;              // x0, y0, z0, x1, y1, z1, ..... ..... ....
    int      *fSegs;              // c0, p0, q0, c1, p1, q1, ..... ..... ....  
    int      *fPols;              // c0, n0, s0, s1, ... sn, c1, n1, s0, ... sn
+};
+
+struct UFacets{
+  UInt_t f1;//number of vertices from verticesList forming a facet
+  UInt_t f2;
+  UInt_t f3;
+  UInt_t f4;
+};
+struct UPolyhedron{
+  std::vector<UVector3>  vertices;//List of Vertices for Polyhedron used in G4Vis
+  std::vector<UFacets>   facets;//List of Facets;
+
 };
 
 typedef std::string UString;
