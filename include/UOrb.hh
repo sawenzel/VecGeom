03989a1662b1d4371c4c1c7d2b46d48298908c9b
marek.gayer <marek.gayer@07d0e867-4f0b-4bfb-81c0-4958028a6c5b>
2011-09-02 08:00:59 +0000

    Migration towards Doxygen comments

--- include/UOrb.hh
+++ include/UOrb.hh
@@ -1,18 +1,20 @@
+/**
+* @file
+* @author  John Doe <jdoe@example.com>
+* @version 0.1
+*
+* @section LICENSE
+*
+* @section DESCRIPTION
+*
+* A simple Orb defined by half-lengths on the three axis. The center of the Orb matches the origin of the local reference frame.
+*/
+
 #ifndef USOLIDS_UOrb
 #define USOLIDS_UOrb
-////////////////////////////////////////////////////////////////////////////////
-//
-//  A simple orb defined by half-lengths on the three axis. The center of the
-//  orb matches the origin of the local reference frame.  
-//
-////////////////////////////////////////////////////////////////////////////////
 
 #include "VUSolid.hh"
 #include "UUtils.hh"
-
-#define _USE_MATH_DEFINES
-#include <math.h>
-
 
 class UOrb : public VUSolid
 {
@@ -23,7 +25,7 @@ public:
    virtual ~UOrb() {}
    
    // Navigation methods
-   EnumInside     Inside (const UVector3 &aPoint) const;   
+   EnumInside     Inside (const UVector3 &aPo6int) const;   
 
    virtual double  SafetyFromInside ( const UVector3 aPoint, 
                                       bool aAccurate=false) const;
@@ -42,11 +44,11 @@ public:
    virtual bool Normal ( const UVector3& aPoint, UVector3 &aNormal ); 
    virtual void Extent ( EAxisType aAxis, double &aMin, double &aMax );
    virtual void Extent ( double aMin[3], double aMax[3] ); 
-   virtual double Capacity() {return (4*M_PI/3)*fR*fR*fR;}
-   virtual double SurfaceArea() {return (4*M_PI)*fR*fR;}
+   virtual double Capacity();
+   virtual double SurfaceArea();
    virtual VUSolid* Clone() const {return 0;}
    virtual UGeometryType GetEntityType() const { return "UOrb";}
-   virtual void    ComputeBBox(UBBox *aBox, bool aStore = false) {}
+   virtual void ComputeBBox(UBBox *aBox, bool aStore = false) {}
 
   //G4Visualisation
    virtual void GetParametersList(int aNumber,double *aArray) const{} 
