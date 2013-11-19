4f6e789105496cf5cb9c9d21882907f19e59f478
Sandro Wenzel <sandro.wenzel@cern.ch>
2013-11-19 10:20:22 +0100

    added header with some global declarations

---
+++ GlobalDefs.h
@@ -0,0 +1,17 @@
+/*
+ * GlobalDefs.h
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: swenzel
+ */
+
+#ifndef GLOBALDEFS_H_
+#define GLOBALDEFS_H_
+
+
+#ifndef ALIGNMENT_BOUNDARY
+#define	ALIGNMENT_BOUNDARY 32
+#endif
+
+
+#endif /* GLOBALDEFS_H_ */
