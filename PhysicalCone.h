0d3223f284e0523be65bc564a15632333ea79064
Sandro Wenzel <sandro.wenzel@cern.ch>
2013-12-11 12:16:02 +0100

    developing some debug methods for cone; just a "transfer" commit

--- PhysicalCone.h
+++ PhysicalCone.h
@@ -32,6 +32,12 @@ private:
 	T cacheTolIRminSqr; // tolerant inner radius of rmin
 	T cacheTolIRmaxSqr; // tolerant inner radius of rmax
 
+	// some cone specific stuff
+	T innerslope; // "gradient" of inner surface in z direction
+	T outerslope; // "gradient" of outer surface in z direction
+	T inneroffset;
+	T outeroffset;
+
 	//**** we save normals to phi - planes *****//
 	Vector3D normalPhi1;
 	Vector3D normalPhi2;
@@ -45,7 +51,10 @@ public:
 		dZ(pDZ),
 		dSPhi(pPhiMin),
 		dDPhi(pPhiMax) {
-			// calculate caches
+
+		// calculate caches
+		// the possible caches are one major difference between tube and cone
+
 			cacheRminSqr=dRmin1*dRmin1;
 			cacheRmaxSqr=dRmax1*dRmax1;
 
@@ -141,8 +150,12 @@ public:
 				   bool /* wantMinusSolution */ ) const ;
 
 	bool isInRRange( Vector3D const & x ) const;
-	bool isInPhiRange( Vector3D const & x) const;
+	bool isInPhiRange( Vector3D const & x ) const;
 	bool isInZRange( Vector3D const & x ) const;
+	Vector3D PhiHit( Vector3D const &x, Vector3D const &y, double & distance, bool isPhi1Solution ) const;
+	Vector3D ZHit( Vector3D const &x, Vector3D const &y, double & distance ) const;
+
+	Vector3D RHit( Vector3D const &x, Vector3D const &y, T & distance, T radius, bool wantMinusSolution ) const;
 	void printInfoHitPoint( char const * c, Vector3D const & vec, double distance ) const;
 };
 
@@ -162,30 +175,36 @@ bool
 PlacedCone<tid,rid, TubeType,T>::isInRRange( Vector3D const & pos) const
 {
 	double d = Vector3D::scalarProductInXYPlane(pos, pos);
-	return ( tubeparams->cacheRminSqr <= d && d <= tubeparams->cacheRmaxSqr );
+
+	// we also need z coordinate for the cone
+	T outerradius = coneparams->outerslope*pos.z+coneparams->outeroffset;
+	T innerradius = coneparams->innerslope*pos.z+coneparams->inneroffset;
+	// we need rad
+
+	return ( innerradius <= d && d <= outerradius );
 }
 
 template <int tid, int rid, class ConeType, typename T>
 Vector3D PlacedCone<tid,rid, ConeType, T>::RHit( Vector3D const & pos, Vector3D const & dir,
 												 T & distance,
-												 T radiusatminusz,
-												 T radiusatplusz,
+												 T m,
+												 T n,
 												 bool wantMinusSolution ) const
 {
-	// "slope" of conic surface
-	T m = -( radiusatminusz - radiusatplusz )/(2*coneparams->dZ);
-	T n = radiusatplusz - m*coneparams->dZ;
+	// m = z-gradient of cone surface
+	// T m = -( radiusatminusz - radiusatplusz )/(2*coneparams->dZ);
+	// T n = radiusatplusz - m*coneparams->dZ;
 
 	T a = 1 - dir.z*dir.z*(1-m*m);
 	T b = 2 * ( pos.x*dir.x + pos.y*dir.y - pos.z*dir.z*m*m - 2*m*n * dir.z);
 	T c = (pos.x*pos.x + pos.y*pos.y - m*m*pos.z*pos.z - 2*m*n*pos.z + n*n);
 
-	T discriminat = b*b - 4*a*c;
+	T discriminant = b*b - 4*a*c;
 
-	if(discriminat >= 0)
+	if(discriminant >= 0)
 	{
-		if( wantMinusSolution ) distance = ( -b - sqrt(discriminat) );
-		if( ! wantMinusSolution ) distance = (-b + sqrt(discriminat) );
+		if( wantMinusSolution ) distance = ( -b - sqrt(discriminant) );
+		if( ! wantMinusSolution ) distance = (-b + sqrt(discriminant) );
 	}
 	else
 	{
@@ -203,6 +222,28 @@ PlacedCone<tid,rid, TubeType,T>::isInPhiRange( Vector3D const & pos) const
 }
 
 template<int tid, int rid, class TubeType, typename T>
+Vector3D
+PlacedCone<tid,rid, TubeType,T>::PhiHit ( Vector3D const & pos, Vector3D const & dir, double & distance, bool isPhi1Solution ) const
+{
+	Vector3D const & normal = ( isPhi1Solution ) ? coneparams->normalPhi1 : coneparams->normalPhi2;
+	T scalarproduct1 = normal.x*pos.x + normal.y*pos.y;
+	T N1dotDir = normal.x*dir.x + normal.y*dir.y;
+
+	distance = -scalarproduct1/N1dotDir;
+	Vector3D vec( pos.x + distance*dir.x, pos.y + distance*dir.y, pos.z + distance*dir.z );
+	return vec;
+}
+
+template<int tid, int rid, class TubeType, typename T>
+Vector3D
+PlacedCone<tid,rid, TubeType, T>::ZHit( Vector3D const & pos, Vector3D const & dir, double & distance ) const
+{
+	distance = - (coneparams->dZ - std::abs(pos.z))/std::abs(dir.z);
+	Vector3D vec( pos.x + distance*dir.x, pos.y + distance*dir.y, pos.z + distance*dir.z );
+	return vec;
+}
+
+template<int tid, int rid, class TubeType, typename T>
 void
 PlacedCone<tid,rid,TubeType,T>::DebugPointAndDirDistanceToIn( Vector3D const & x, Vector3D const & y ) const
 {
@@ -213,13 +254,13 @@ PlacedCone<tid,rid,TubeType,T>::DebugPointAndDirDistanceToIn( Vector3D const & x
 	std::cout << "PARTICLE DIRECTION " << std::endl;
 	std::cout << y << std::endl;
 	std::cout << "RMAX INTERACTION " << std::endl;
-	Vector3D vmax = this->RHit( x, y, distance, coneparams->dRmax2, coneparams->dRmax1, true);
+	Vector3D vmax = this->RHit( x, y, distance, coneparams->outerslope, coneparams->outeroffset, true);
 	printInfoHitPoint("hitpointrmax", vmax, distance);
 
 	if( ConeTraits::NeedsRminTreatment<TubeType>::value )
 	{
 		std::cout << "RMIN INTERACTION " << std::endl;
-		Vector3D vmin = this->RHit( x, y, distance, tubeparams->dRmin, false);
+		Vector3D vmin = this->RHit( x, y, distance, coneparams->innerslope, coneparams->inneroffset, false );
 		printInfoHitPoint("hitpointrmin", vmin, distance);
 	}
 
