9a2bbe470c4860780704f173b4ab045bf744b373
Johannes de Fine Licht <johannes@musicmedia.dk>
2013-12-12 15:07:22 +0100

    Initialize with USolid and ROOT solid representation

--- Tests/compilescript4.sh
+++ Tests/compilescript4.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -fabi-version=6 -m64 -std=c++11 `root-config --cflags --libs` -I ../ ShapeTesterTester.cpp ../*.cpp -o bin/fastgeom -lGeom -lVc -ltbb -lrt
+g++ -fabi-version=6 -m64 -std=c++11 `root-config --cflags --libs` -I ../ ShapeTesterTester.cpp ../*.cpp -o bin/fastgeom -lGeom -lVc -ltbb -lrt -lusolids
