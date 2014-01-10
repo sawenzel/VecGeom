e626e9fed751a0562b1d2f56d20dd9fc23261bd5
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-01-10 08:09:02 +0100

    test for tube contains function

--- Tests/TestTubeContains.cpp
+++ Tests/TestTubeContains.cpp
@@ -1,12 +1,24 @@
+/*
+ * TestVectorizedPlacedTube.cpp
+ *
+ *  Created on: Nov 18, 2013
+ *      Author: swenzel
+ */
+
 // testing matrix transformations on vectors
 #include "../TransformationMatrix.h"
+#include "TGeoMatrix.h"
+#include "TGeoManager.h"
+#include "TGeoVolume.h"
+#include "TGeoShape.h"
 #include "../Utils.h"
 #include <iostream>
 #include "mm_malloc.h"
 #include "../GlobalDefs.h"
 #include "../GeoManager.h"
-#include "../PhysicalPolycone.h"
+#include "../PhysicalTube.h"
 #include "../TestShapeContainer.h"
+#include "../SimpleVecNavigator.h"
 
 // in order to compare to USolids
 #include "VUSolid.hh"
@@ -37,8 +49,8 @@ int main()
 	Vectors3DSOA points, intermediatepoints;
 	StructOfCoord rpoints, rintermediatepoints;
 
-	int np = 1024000;
-	int NREPS = 10;
+	int np = 1024;
+	int NREPS = 1000;
 
 	points.alloc(np);
 	intermediatepoints.alloc(np);
@@ -53,31 +65,21 @@ int main()
 
 	StopWatch timer;
 
+    // generate benchmark cases
+	TransformationMatrix const * identity = new TransformationMatrix(0,0,0,0,0,0);
 
 	// the world volume is a tube
 	double worldrmax = 100.;
 	double worldrmin = 0.;
 	double worldz = 200.;
-	PhysicalVolume * world = GeoManager::MakePlacedTube( new TubeParameters<>(worldrmin, worldrmax, worldz, 0, M_PI ), IdentityTransformationMatrix );
+	PhysicalVolume * world = GeoManager::MakePlacedTube( new TubeParameters<>(worldrmin, worldrmax, worldz, 0, 2.*M_PI), identity );
 
-	double zplanes[] = {-80, -20, 20, 80 };
-	double rinner[] = { 0, 0, 0, 0 };
-	double router[] = { 70, 70, 100, 100 };
-
-/*
-	double zplanes[] = {-80, 80 };
-	double rinner[] = { 0, 10, };
-	double router[] = { 50, 80, };
-*/
-
-	PolyconeParameters<> * pconp = new PolyconeParameters<>( 0, 2.*M_PI, 4, zplanes, rinner, router );
-
-	PhysicalVolume * pcon = GeoManager::MakePlacedPolycone(pconp, IdentityTransformationMatrix, true );
+	PhysicalVolume * shield = GeoManager::MakePlacedTube( new TubeParameters<>(0, 5*worldrmax/10, 8*worldz/10, 0, 2. * M_PI ), identity );
 
 	// fill points with no daughter inside; then place daughter
 	world->fillWithRandomPoints(points,np);
 
-	world->AddDaughter( pcon );
+	world->AddDaughter( shield );
 
 	points.toPlainArray(plainpointarray,np);
 
@@ -86,7 +88,7 @@ int main()
 	for(int reps=0 ;reps < NREPS; reps++ )
 	{
 		for(auto particle = 0; particle < np; ++particle )
-			results[particle] = pcon->Contains_WithSurface( points.getAsVector(particle) );
+			results[particle] = shield->Contains_WithSurface( points.getAsVector(particle) );
 	}
 	timer.Stop();
 	double t0 = timer.getDeltaSecs();
@@ -98,24 +100,16 @@ int main()
 		for( auto particle = 0; particle < np; ++particle )
 			{
 				Vector3D pos = points.getAsVector(particle);
-				resultsUsolid[particle] = pcon->GetAsUnplacedUSolid()->Inside( reinterpret_cast<UVector3 const &>( pos  ) );
+				resultsUsolid[particle] = shield->GetAsUnplacedUSolid()->Inside( reinterpret_cast<UVector3 const &>( pos  ) );
 			}
 		}
 	timer.Stop();
 	double t1 = timer.getDeltaSecs();
 	std::cerr << t1 << std::endl;
 
-	int counter[3]={0,0,0};
-	int counterUSolid[3]={0,0,0};
 	for( auto particle = 0; particle < np; ++particle )
 	{
-		counter[results[particle]]++;
-		counterUSolid[resultsUsolid[particle]]++;
-	}
-	for( auto j=0; j < 3; j++)
-	{
-		std::cerr << counter[j] << " " << counterUSolid[j] << std::endl;
-
+		std::cerr << results[particle] << " " << resultsUsolid[particle] << std::endl;
 	}
 
     return 1;
