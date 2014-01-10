9fa45d7566e032753c124a7631e7233876bdde85
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-01-10 08:02:38 +0100

    added more polycone test cases

--- PhysicalCone.h
+++ PhysicalCone.h
@@ -797,7 +797,7 @@ void PlacedCone<tid,rid,ConeType,ValueType>::DistanceToIn( VectorType const & xm
 
 	// now PHI
 
-	// **** PHI TREATMENT FOR CASE OF HAVING RMAX ONLY ***** only compiled in for tubes having phi sektion ***** //
+	// **** PHI TREATMENT FOR CASE OF HAVING RMAX ONLY ***** only compiled in for cones having phi sektion ***** //
 	if ( ConeTraits::NeedsPhiTreatment<ConeType>::value )
 	{
 		// all particles not done until here have the potential to hit a phi surface
--- Tests/TestPolyconeContains.cpp
+++ Tests/TestPolyconeContains.cpp
@@ -60,32 +60,56 @@ int main()
 	double worldz = 200.;
 	PhysicalVolume * world = GeoManager::MakePlacedTube( new TubeParameters<>(worldrmin, worldrmax, worldz, 0, M_PI ), IdentityTransformationMatrix );
 
-	double zplanes[] = {-80, -20, 20, 80 };
-	double rinner[] = { 0, 0, 0, 0 };
-	double router[] = { 70, 70, 100, 100 };
+	/*
+		// a small polycone
+		double zplanes[] = {-80, -20, 20, 80 };
+		double rinner[] = { 0, 0, 0, 0 };
+		double router[] = { 70, 70, 100, 100 };
+		int Nz=4;
+	*/
 
-/*
+	/*
+		// a medium polycone
+		double zplanes[] = {-90, -70, -50, -30, -10, 0, 10, 30, 50, 70, 90 };
+		double rinner[] =  { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+		double router[] =  { 70, 70, 100, 100, 70, 70, 70, 100, 100, 70, 70 };
+		const int Nz=11;
+	*/
+
+	/*
+	// a large polycone
+		const int Nz=101;
+		for(auto i = 0;i< Nz; i++)
+		{
+			zplanes[i]=-90 + i*(180./(Nz-1));
+			rinner[i]=5;
+			if(i%4==0) router[i]=70;
+		 	if(i%4==1) router[i]=70;
+		 	if(i%4==2) router[i]=100;
+		 	if(i%4==3) router[i]=100;
+		}
+    */
+
+	/*
+	// a minimal polycone
 	double zplanes[] = {-80, 80 };
 	double rinner[] = { 0, 10, };
 	double router[] = { 50, 80, };
-*/
+	const int Nz=2;
+	 */
 
-	PolyconeParameters<> * pconp = new PolyconeParameters<>( 0, 2.*M_PI, 4, zplanes, rinner, router );
-
+	PolyconeParameters<> * pconp = new PolyconeParameters<>( 0, 2.*M_PI, Nz, zplanes, rinner, router );
 	PhysicalVolume * pcon = GeoManager::MakePlacedPolycone(pconp, IdentityTransformationMatrix, true );
 
 	// fill points with no daughter inside; then place daughter
 	world->fillWithRandomPoints(points,np);
-
 	world->AddDaughter( pcon );
-
 	points.toPlainArray(plainpointarray,np);
-
 
 	timer.Start();
 	for(int reps=0 ;reps < NREPS; reps++ )
 	{
-		for(auto particle = 0; particle < np; ++particle )
+		for( auto particle = 0; particle < np; ++particle )
 			results[particle] = pcon->Contains_WithSurface( points.getAsVector(particle) );
 	}
 	timer.Stop();
@@ -107,7 +131,7 @@ int main()
 
 	int counter[3]={0,0,0};
 	int counterUSolid[3]={0,0,0};
-	for( auto particle = 0; particle < np; ++particle )
+	for( auto particle=0; particle<np; ++particle )
 	{
 		counter[results[particle]]++;
 		counterUSolid[resultsUsolid[particle]]++;
