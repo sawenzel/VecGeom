b695f966b72f62b20e6112f26e0ebbffcbd74cdb
Johannes de Fine Licht <johannes.definelicht@cernch>
2014-01-24 15:19:31 +0100

    Cilk backend implemented and working, all three backends having identical output.

--- cuda_prototype/CompareOutput.cpp
+++ cuda_prototype/CompareOutput.cpp
@@ -9,22 +9,27 @@ int main(const int argc, const char * const argv[]) {
     return -1;
   }
   std::string filename(argv[1]);
-  std::ifstream file_cuda, file_vc;
+  std::ifstream file_cuda, file_vc, file_cilk;
   file_cuda.open(filename + ".out.cuda");
   file_vc.open(filename + ".out.vc");
+  file_cilk.open(filename + ".out.cilk");
   int count = 0;
   int equal = 0;
-  while (file_cuda && file_vc) {
+  while (file_cuda && file_vc && file_cilk) {
     count++;
-    std::string val_cuda, val_vc;
+    std::string val_cuda, val_vc, val_cilk;
     std::getline(file_cuda, val_cuda);
     std::getline(file_vc, val_vc);
+    std::getline(file_cilk, val_cilk);
     const double cuda = std::atof(val_cuda.c_str());
     const double vc = std::atof(val_vc.c_str());
-    if (std::fabs(cuda - vc) < 1e-12 || cuda == vc) {
+    const double cilk = std::atof(val_cilk.c_str());
+    if ((std::fabs(cuda - vc) < 1e-12 || cuda == vc) &&
+        (std::fabs(vc - cilk) < 1e-12 || vc == cilk)) {
       equal++;
     } else {
-      // std::cout << "Mismatch: " << vc << " / " << cuda << std::endl;
+      std::cout << "Mismatch: " << vc << " / " << cuda << " / "
+                << cilk << std::endl;
     }
   }
   std::cout << (count - equal) << " / " << count << " mismatches detected.\n";
--- cuda_prototype/ImplementationsCilk.cpp
+++ cuda_prototype/ImplementationsCilk.cpp
@@ -3,30 +3,54 @@
 #include "Box.h"
 #include "Tube.h"
 
-const CilkBool Impl<kCilk>::kTrue = CilkBool(true);
-const CilkBool Impl<kCilk>::kFalse = CilkBool(false);
-const CilkFloat Impl<kCilk>::kZero = CilkFloat(0);
-
 void Box::DistanceToIn(SOA3D<double> const &pos,
                        SOA3D<double> const &dir,
                        double const *steps_max,
                        double *distance) const {
 
   const int size = pos.size();
-  const Vector3D<CilkFloat> pos_cilk(
-    CilkFloat(pos.x(), size, false),
-    CilkFloat(pos.y(), size, false),
-    CilkFloat(pos.z(), size, false)
-  );
-  const Vector3D<CilkFloat> dir_cilk(
-    CilkFloat(dir.x(), size, false),
-    CilkFloat(dir.y(), size, false),
-    CilkFloat(dir.z(), size, false)
-  );
-  const CilkFloat step_max(steps_max, size, false);
-  CilkFloat output(distance, size, false);
-  kernel::box::DistanceToIn<kCilk>(
-    dimensions, trans_matrix, pos_cilk, dir_cilk, step_max, output
-  );
+  constexpr int vec_size = CilkFloat::VecSize();
+  const int i_max = size - (size % vec_size);
+  for (int i = 0; i < i_max; i += vec_size) {
+    const Vector3D<CilkFloat> pos_cilk(
+      CilkFloat(&pos.x(i)),
+      CilkFloat(&pos.y(i)),
+      CilkFloat(&pos.z(i))
+    );
+    const Vector3D<CilkFloat> dir_cilk(
+      CilkFloat(&dir.x(i)),
+      CilkFloat(&dir.y(i)),
+      CilkFloat(&dir.z(i))
+    );
+    const CilkFloat step_max(steps_max);
+    CilkFloat output;
+    kernel::box::DistanceToIn<kCilk>(
+      parameters->dimensions, trans_matrix, pos_cilk, dir_cilk, step_max, output
+    );
+    output.Store(&distance[i]);
+  }
+  for (int i = i_max; i < size; ++i) {
+    kernel::box::DistanceToIn<kScalar>(
+      parameters->dimensions, trans_matrix, pos[i], dir[i], steps_max[i],
+      distance[i]
+    );
+  }
 
+}
+
+void Box::Contains(SOA3D<double> const &points,
+                   bool *output) const {
+  // NYI
+}
+
+void Tube::Contains(SOA3D<double> const &points,
+                    bool *output) const {
+  // NYI
+}
+
+void Tube::DistanceToIn(SOA3D<double> const &pos,
+                        SOA3D<double> const &dir,
+                        double const *steps_max,
+                        double *distance) const {
+  // NYI
 }
