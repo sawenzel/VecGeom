619da30ae2ff0547a30218ed035c1432041e8a49
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-02-13 16:03:00 +0100

    extensions of fast vector class; introduction of fast vectorized transforms ( for 1-particle case )

---
+++ prototypev1/TestVectorFast.cpp
@@ -0,0 +1,120 @@
+#include "Vector3DFast.h"
+#include "Vector3D.h"
+#include <iostream>
+#include "Vc/vector.h"
+
+bool containsold( Vector3D const & point, Vector3D const & p ) 
+{
+  if ( std::abs(point.x) > p.x ) return false;
+  if ( std::abs(point.y) > p.y ) return false;
+  if ( std::abs(point.z) > p.z ) return false;
+  return true; 
+}
+
+
+bool containsnew( Vector3DFast const & point, Vector3DFast const & p ) 
+{
+  Vector3DFast tmp = point.Abs();
+  return ! tmp.IsAnyLargerThan( p ); 
+}
+
+
+bool transform( FastTransformationMatrix const & m, Vector3DFast const & master, Vector3DFast & local )
+{
+  m.MasterToLocal( master,local );
+}
+
+
+void abs0( Vc::double_v const & a, Vc::double_v  &b )
+{
+  b=Vc::abs( a );
+}
+
+void abs1( Vector3DFast const & a, Vector3DFast &b )
+{
+  b=a.Abs();
+}
+
+void abs2( Vector3D const & a, Vector3D &b )
+{
+  b.x=std::abs(a.x);
+  b.y=std::abs(a.y);
+  b.z=std::abs(a.z);
+}
+
+void ass1( Vector3DFast const & a, Vector3DFast &b )
+{
+  b=a;
+}
+
+
+void foo( Vector3D const & a, Vector3D &b )
+{
+  b+=a;
+  b*=a;
+  b+=a;
+  b/=a;
+}
+
+void bar( Vector3DFast const & a, Vector3DFast &b )
+{
+  b+=a;
+  b*=a;
+  b+=a;
+  b/=a;
+}
+
+double baz( Vector3DFast const & a, Vector3DFast const &b )
+{
+  return a.ScalarProduct(b);
+}
+
+double xxx( Vector3D const & a, Vector3D const &b )
+{
+  return Vector3D::scalarProduct(a,b);
+}
+
+
+int main()
+{
+  Vector3DFast x(1,2,3);
+  Vector3DFast y(1,2,3);
+  Vector3D x2(1,2,3);
+  Vector3D y2(1,2,3);
+
+  foo(x2,y2);
+  bar(x,y);
+
+  double z = baz(x,y);
+
+  std::cerr << y2 << std::endl;
+  std::cerr << y << std::endl;
+  std::cerr << y << std::endl;
+
+  // test new contains functionality
+  Vector3DFast p1( -20,9,9 );
+  Vector3DFast p2( 9,9,9 );
+  Vector3D p1o( -20,9,9 );
+  Vector3D p2o( 9,9,9 );
+
+  Vector3DFast para(10,10,10);
+  Vector3D parao(10,10,10);
+
+  std::cerr << containsold(p1o, parao)  << " " << containsnew(p1, para) << std::endl;
+  std::cerr << containsold(p2o, parao)  << " " << containsnew(p2, para) << std::endl;
+
+  FastTransformationMatrix m(0,0,0,0,30,45);
+  m.print();
+  Vector3DFast a(1,0,0);
+  Vector3DFast b(1,0,0);
+  Vector3DFast tmp;
+
+  m.LocalToMaster(a, b);
+  std::cerr << b << std::endl;  
+
+  m.MasterToLocal(b, tmp);
+  std::cerr << tmp << std::endl;  
+
+
+  return 1;
+}
--- prototypev1/Vector3DFast.h
+++ prototypev1/Vector3DFast.h
@@ -16,35 +16,92 @@
 class Vector3DFast
 {
 private:
-	Vc::Memory<Vc::Vector<double >, 3 > internalVcmemory;
+	typedef Vc::Vector<double> base_t;
+	typedef typename base_t::Mask mask_t;
+	Vc::Memory<base_t, 3 > internalVcmemory;
+
+	inline
+	void SetVector(int i, Vc::Vector<double > const & v)
+	{
+		internalVcmemory.vector(i)=v;
+	}
 
 public:
-	Vector3DFast( double x,  double y,  double z) : internalVcmemory() {
-		SetX(x);SetY(y);SetZ(z);
+	inline
+	Vector3DFast( ) : internalVcmemory() {
+
+		}
+
+	inline
+	Vector3DFast( Vector3DFast const & rhs ) : internalVcmemory() {
+		for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
+			{
+				this->internalVcmemory.vector(i)=rhs.internalVcmemory.vector(i);
+			}
 	}
+
+
+	inline
+	Vector3DFast( double x,  double y,  double z) : internalVcmemory() {
+			SetX(x);SetY(y);SetZ(z);
+		}
+
+
+	inline
+	__attribute__((always_inline))
+	Vector3DFast & operator+=( Vector3DFast const & rhs )
+	{
+		this->internalVcmemory+=rhs.internalVcmemory;
+		return *this;
+	}
+
+
+	inline
+	__attribute__((always_inline))
+	Vector3DFast & operator*=( double s )
+	{
+		this->internalVcmemory*=s;
+		return *this;
+	}
+
 
 	inline
 		__attribute__((always_inline))
-		Vector3DFast & operator+=( Vector3DFast const & rhs )
-			{
-				this->internalVcmemory+=rhs.internalVcmemory;
-				return *this;
-			}
+		Vector3DFast & operator-=( Vector3DFast const & rhs )
+		{
+			this->internalVcmemory-=rhs.internalVcmemory;
+			return *this;
+		}
 
-		inline
-			__attribute__((always_inline))
-			Vector3DFast & operator*=( Vector3DFast const & rhs )
+
+
+	// assignment operator
+	inline
+	__attribute__((always_inline))
+	Vector3DFast & operator=( Vector3DFast const & rhs )
+	{
+		for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
+		{
+			this->internalVcmemory.vector(i)=rhs.internalVcmemory.vector(i);
+			return *this;
+		}
+	}
+
+
+	inline
+	__attribute__((always_inline))
+	Vector3DFast & operator*=( Vector3DFast const & rhs )
+	{
+		/*
+			for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
 			{
-			/*
-					for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
-						{
-					//		std::cerr << "adding vector " << i <<  std::endl;
-						this->internalVcmemory.vector(i) *= rhs.internalVcmemory.vector(i);
-					}
-					*/
-				this->internalVcmemory*=rhs.internalVcmemory;
-				return *this;
+				//		std::cerr << "adding vector " << i <<  std::endl;
+				this->internalVcmemory.vector(i) *= rhs.internalVcmemory.vector(i);
 			}
+				*/
+			this->internalVcmemory*=rhs.internalVcmemory;
+			return *this;
+	}
 
 
 	inline
@@ -94,8 +151,110 @@ public:
 
 	inline void SetZ(double z)  {
 		internalVcmemory[2]=z;}
+
+
+	inline
+	__attribute__((always_inline))
+	Vector3DFast Abs() const
+	{
+		Vector3DFast tmp;
+		for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
+		{
+			base_t v = this->internalVcmemory.vector(i);
+			tmp.internalVcmemory.vector(i) = Vc::abs( v );
+		}
+		return tmp;
+	}
+
+	inline
+	__attribute__((always_inline))
+	// a starting comparison operator
+	bool IsAnySmallerZero( ) const
+	{
+		bool result = false;
+		Vector3DFast tmp;
+		for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
+		{
+			mask_t m = this->internalVcmemory.vector(i) < 0;
+			result |= ! m.isEmpty();
+		}
+		return result;
+	}
+
+	inline
+	__attribute__((always_inline))
+	// a starting comparison operator
+	bool IsAnySmallerThan( Vector3DFast const & rhs ) const
+	{
+		bool result = false;
+		for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
+		{
+			base_t v1,v2;
+			v1 = this->internalVcmemory.vector(i);
+			v2 = rhs.internalVcmemory.vector(i);
+			mask_t m =  v1 < v2;
+			result |= ! m.isEmpty();
+// could early return here ...
+
+		}
+		return result;
+	}
+
+	inline
+	__attribute__((always_inline))
+	// a starting comparison operator
+	bool IsAnyLargerThan( Vector3DFast const & rhs ) const
+	{
+			bool result = false;
+			for( int i=0; i < 1 + 3/Vc::Vector<double>::Size; i++ )
+			{
+				base_t v1,v2;
+				v1 = this->internalVcmemory.vector(i);
+				v2 = rhs.internalVcmemory.vector(i);
+				mask_t m =  v1 > v2;
+				result |= ! m.isEmpty();
+// could early return here
+			}
+			return result;
+	}
+
+
 };
 
+
+inline
+Vector3DFast const operator-(Vector3DFast const & lhs, Vector3DFast const & rhs)
+{
+	Vector3DFast tmp(lhs);
+	tmp-=rhs;
+	return tmp;
+}
+
+
+inline
+Vector3DFast const operator+(Vector3DFast const & lhs, Vector3DFast const & rhs)
+{
+	Vector3DFast tmp(lhs);
+	tmp+=rhs;
+	return tmp;
+}
+
+
+inline
+Vector3DFast const operator*(double lhs, Vector3DFast const & rhs)
+{
+	Vector3DFast tmp(rhs);
+	tmp*=lhs;
+	return tmp;
+}
+
+inline
+Vector3DFast const operator*(Vector3DFast const & lhs, double rhs)
+{
+	Vector3DFast tmp(lhs);
+	tmp*=rhs;
+	return tmp;
+}
 
 
 inline
@@ -106,6 +265,91 @@ std::ostream& operator<<( std::ostream& stream, Vector3DFast const & vec )
 	return stream;
 }
 
+// to try something with Vc memory
+class FastTransformationMatrix
+{
+// storage
+private:
+    // the translation
+	Vector3DFast trans;
 
+	// the columns of the rotation matrix ( as vectors )
+	Vector3DFast rotcol1;
+	Vector3DFast rotcol2;
+	Vector3DFast rotcol3;
+
+	// the rows of the rotation matrix ( as vectors )
+	Vector3DFast rotrow1;
+	Vector3DFast rotrow2;
+	Vector3DFast rotrow3;
+
+public:
+	FastTransformationMatrix() : trans(), rotcol1(), rotcol2(), rotcol3(),
+		rotrow1(), rotrow2(), rotrow3()
+	{};
+
+		//template <TranslationIdType tid, RotationIdType rid>
+	inline
+	void
+	MasterToLocal(Vector3DFast const & master, Vector3DFast & local) const
+	{
+		Vector3DFast tmp = master-trans;
+		local = tmp.GetX()*rotrow1 + tmp.GetY()*rotrow2 + tmp.GetZ()*rotrow3;
+	}
+
+	inline
+	void
+	LocalToMaster(Vector3DFast const & local, Vector3DFast & master) const
+	{
+		Vector3DFast tmp = local+trans;
+		master = tmp.GetX()*rotcol1 + tmp.GetY()*rotcol2 + tmp.GetZ()*rotcol3;
+	}
+
+	void setAngles(double phi, double theta, double psi)
+	{
+		double degrad = M_PI/180.;
+		double sinphi = sin(degrad*phi);
+		double cosphi = cos(degrad*phi);
+		double sinthe = sin(degrad*theta);
+		double costhe = cos(degrad*theta);
+		double sinpsi = sin(degrad*psi);
+		double cospsi = cos(degrad*psi);
+
+		// this is setting the rowvectors
+		rotrow1.SetX( cospsi*cosphi - costhe*sinphi*sinpsi );
+		rotrow1.SetY( -sinpsi*cosphi - costhe*sinphi*cospsi );
+		rotrow1.SetZ( sinthe*sinphi );
+
+		rotrow2.SetX( cospsi*sinphi + costhe*cosphi*sinpsi );
+		rotrow2.SetY( -sinpsi*sinphi + costhe*cosphi*cospsi );
+		rotrow2.SetZ( -sinthe*cosphi );
+
+		rotrow3.SetX( sinpsi*sinthe );
+		rotrow3.SetY( cospsi*sinthe );
+		rotrow3.SetZ( costhe );
+
+		rotcol1.SetX( rotrow1.GetX() ); rotcol1.SetY( rotrow2.GetX() ); rotcol1.SetZ( rotrow3.GetX() );
+		rotcol2.SetX( rotrow1.GetY() ); rotcol2.SetY( rotrow2.GetY() ); rotcol2.SetZ( rotrow3.GetY() );
+		rotcol3.SetX( rotrow1.GetZ() ); rotcol3.SetY( rotrow2.GetZ() ); rotcol3.SetZ( rotrow3.GetZ() );
+	}
+
+
+	FastTransformationMatrix(double tx, double ty, double tz, double phi, double theta, double psi)
+	{
+		trans.SetX(tx);
+		trans.SetY(ty);
+		trans.SetZ(tz);
+
+		setAngles(phi, theta, psi);
+	}
+
+	void print() const
+	{
+		// print the matrix in 4x4 format
+		printf("%12.11f\t%12.11f\t%12.11f    Tx = %10.6f\n", rotcol1.GetX(), rotcol2.GetX(), rotcol3.GetX(), trans.GetX());
+		printf("%12.11f\t%12.11f\t%12.11f    Ty = %10.6f\n", rotcol1.GetY(), rotcol2.GetY(), rotcol3.GetY(), trans.GetY());
+		printf("%12.11f\t%12.11f\t%12.11f    Tz = %10.6f\n", rotcol1.GetZ(), rotcol2.GetZ(), rotcol3.GetZ(), trans.GetZ());
+	}
+};
 
 #endif /* VECTOR3DFAST_H_ */
