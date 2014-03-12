2f988db137d76bb828d28a3b05b1805f75b0090c
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-03-12 17:20:32 +0100

    added stub classes for navigation

---
+++ navigation/simple_navigator.h
@@ -0,0 +1,14 @@
+/*
+ * simple_navigator.h
+ *
+ *  Created on: Mar 12, 2014
+ *      Author: swenzel
+ */
+
+#ifndef SIMPLE_NAVIGATOR_H_
+#define SIMPLE_NAVIGATOR_H_
+
+
+
+
+#endif /* SIMPLE_NAVIGATOR_H_ */
