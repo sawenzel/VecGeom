3f46d2264b0fda48250cf9d4834ecd74261d62be
Johannes de Fine Licht <johannes.definelicht@cernch>
2014-03-19 11:22:38 +0100

    Types file merged into global.

--- base/types.h
+++
@@ -1,8 +0,0 @@
-/**
- * @file types.h
- * @author Johannes de Fine Licht (johannes.definelicht@cern.ch)
- */
-
-#ifndef VECGEOM_BASE_TYPES_H_
-#define VECGEOM_BASE_TYPES_H_
-#endif // VECGEOM_BASE_TYPES_H_
