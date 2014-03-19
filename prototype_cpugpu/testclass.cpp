776981f255135358dceb1063fa61517e1deee3c3
Johannes de Fine Licht <johannes@musicmedia.dk>
2014-03-19 17:27:26 +0100

    Fixed output.

--- prototype_cpugpu/testclass.cpp
+++ prototype_cpugpu/testclass.cpp
@@ -4,7 +4,7 @@
 namespace CPUGPU_NAMESPACE {
 
 void TestClass::Print() const {
-  for (int i = 0; i < 10; ++i) std::cout << i << " ";
+  for (int i = 0; i < 10; ++i) std::cout << vector[i] << " ";
   std::cout << std::endl;
 }
 
