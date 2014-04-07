62ceb322bb3218c40b96707f729f52343955694a
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-04-07 17:36:42 +0200

    implement very basic query API on GeoManager to get lists of (placed) volumes; using Visitor pattern

--- management/geo_manager.h
+++ management/geo_manager.h
@@ -7,8 +7,50 @@
 #define VECGEOM_MANAGEMENT_GEOMANAGER_H_
 
 #include "base/global.h"
+#include "volumes/logical_volume.h"
+#include "volumes/placed_volume.h"
 
 namespace VECGEOM_NAMESPACE {
+
+// probably don't need apply to be virtual
+template<typename Container>
+class GeoVisitor
+{
+protected:
+	Container & c_;
+public:
+	GeoVisitor( Container & c ) : c_(c) {};
+
+	virtual void apply( VPlacedVolume * ) = 0;
+	virtual ~GeoVisitor(){}
+};
+
+template<typename Container>
+class SimpleLogicalVolumeVisitor : public GeoVisitor<Container>
+{
+public:
+	SimpleLogicalVolumeVisitor( Container & c ) : GeoVisitor<Container>(c) {}
+	virtual void apply( VPlacedVolume * vol ){
+		LogicalVolume const *lvol = vol->logical_volume();
+		if( std::find( this->c_.begin(), this->c_.end(), lvol ) == this->c_.end() )
+		{
+			this->c_.push_back( const_cast<LogicalVolume *>(lvol) );
+		}
+	}
+	virtual ~SimpleLogicalVolumeVisitor(){}
+};
+
+template<typename Container>
+class SimplePlacedVolumeVisitor : public GeoVisitor<Container>
+{
+public:
+	SimplePlacedVolumeVisitor( Container & c) : GeoVisitor<Container>(c) {}
+	virtual void apply( VPlacedVolume * vol ){
+		this->c_.push_back( vol );
+	}
+	virtual ~SimplePlacedVolumeVisitor(){}
+};
+
 
 /**
  * @brief Knows about the current world volume.
@@ -19,6 +61,9 @@ private:
 
   int volume_count;
   VPlacedVolume const *world_;
+
+  template<typename Visitor>
+  void visitAllPlacedVolumes(VPlacedVolume const *, Visitor * visitor) const;
 
 public:
 
@@ -31,20 +76,66 @@ public:
 
   VPlacedVolume const* world() const { return world_; }
 
+  /**
+   *  give back container containing all logical volumes in detector
+   *  Container is supposed to be any Container that can store pointers to
+   */
+  template<typename Container>
+  void getAllLogicalVolumes( Container & c ) const;
+
+  /**
+   *  give back container containing all logical volumes in detector
+   */
+  template<typename Container>
+  void getAllPlacedVolumes( Container & c ) const;
+
 protected:
 
-  friend VPlacedVolume;
-
-  int RegisterVolume(VPlacedVolume const *const volume);
+  // friend VPlacedVolume;
+  // int RegisterVolume(VPlacedVolume const *const volume);
 
 private:
-
   GeoManager() : volume_count(0) {}
 
   GeoManager(GeoManager const&);
   GeoManager& operator=(GeoManager const&);
-
 };
+
+template<typename Visitor>
+void
+GeoManager::visitAllPlacedVolumes( VPlacedVolume const * currentvolume, Visitor * visitor ) const
+{
+	visitor->apply( const_cast<VPlacedVolume *>(currentvolume) );
+	int size = currentvolume->daughters().size();
+	for( int i=0; i<size; ++i )
+	{
+		visitAllPlacedVolumes( currentvolume->daughters().operator[](i), visitor );
+	}
+}
+
+
+template<typename Container>
+void GeoManager::getAllLogicalVolumes( Container & c ) const
+{
+	c.clear();
+	// walk all the volume hierarchy and insert
+	// logical volume if not already in the container
+	SimpleLogicalVolumeVisitor<Container> lv(c);
+	visitAllPlacedVolumes( world(), &lv );
+}
+
+
+template<typename Container>
+void GeoManager::getAllPlacedVolumes( Container & c ) const
+{
+	c.clear();
+	// walk all the volume hierarchy and insert
+	// placed volumes if not already in the container
+	SimplePlacedVolumeVisitor<Container> pv(c);
+	visitAllPlacedVolumes( world(), &pv );
+}
+
+
 
 } // End global namespace
 
