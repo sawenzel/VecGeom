452bc9fcd694af61bad57d87d4cb566a6f8b3789
Johannes de Fine Licht <johannes.definelicht@cernch>
2014-04-10 15:10:21 +0200

    Extended SOA and AOS interface to allow push_back and to modify current fill size when elements are added [VECGEOM-16]. Fixed wrong allocation flag being set in GPU constructor.

--- base/aos3d.h
+++ base/aos3d.h
@@ -14,6 +14,8 @@
 #include "backend/cuda/interface.h"
 #endif
 
+// #include <cassert>
+
 namespace VECGEOM_NAMESPACE {
 
 template <typename Type>
@@ -52,7 +54,7 @@ public:
 
   VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_INLINE
-  Type& x(const int index) { return (data_[index])[0]; }
+  Type* data() { return data_; }
 
   VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_INLINE
@@ -60,15 +62,7 @@ public:
 
   VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_INLINE
-  Type& y(const int index) { return (data_[index])[1]; }
-
-  VECGEOM_CUDA_HEADER_BOTH
-  VECGEOM_INLINE
   Type const& y(const int index) const { return (data_[index])[1]; }
-
-  VECGEOM_CUDA_HEADER_BOTH
-  VECGEOM_INLINE
-  Type& z(const int index) { return (data_[index])[2]; }
 
   VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_INLINE
@@ -82,6 +76,14 @@ public:
   VECGEOM_INLINE
   void Set(const int index, Vector3D<Type> const &vec);
 
+  VECGEOM_CUDA_HEADER_BOTH
+  VECGEOM_INLINE
+  void push_back(const Type x, const Type y, const Type z);
+
+  VECGEOM_CUDA_HEADER_BOTH
+  VECGEOM_INLINE
+  void push_back(Vector3D<Type> const &vec);
+
   #ifdef VECGEOM_CUDA
   AOS3D<Type>* CopyToGpu(Vector3D<Type> *const data_gpu) const;
   #endif
@@ -89,24 +91,28 @@ public:
 };
 
 template <typename Type>
-AOS3D<Type>::AOS3D(Vector3D<Type> *const data, const int size)
-    : TrackContainer<Type>(size, false), data_(data) {}
+AOS3D<Type>::AOS3D(Vector3D<Type> *const data, const int memory_size)
+    : TrackContainer<Type>(memory_size, false), data_(data) {
+  this->size_ = memory_size;
+}
 
 template <typename Type>
-AOS3D<Type>::AOS3D(const int size) : TrackContainer<Type>(size, true){
+AOS3D<Type>::AOS3D(const int memory_size)
+    : TrackContainer<Type>(memory_size, true) {
   data_ = static_cast<VecType*>(
-            _mm_malloc(sizeof(VecType)*size, kAlignmentBoundary)
+            _mm_malloc(sizeof(VecType)*memory_size, kAlignmentBoundary)
           );
-  for (int i = 0; i < size; ++i) new(data_+i) VecType;
+  for (int i = 0; i < memory_size; ++i) new(data_+i) VecType;
 }
 
 template <typename Type>
 AOS3D<Type>::AOS3D(TrackContainer<Type> const &other)
-    : TrackContainer<Type>(other.size_, true) {
+    : TrackContainer<Type>(other.memory_size_, true) {
+  this->size_ = other.size_;
   data_ = static_cast<VecType*>(
             _mm_malloc(sizeof(VecType)*other.size(), kAlignmentBoundary)
           );
-  const unsigned count = other.size();
+  const int count = other.size();
   for (int i = 0; i < count; ++i) {
     data_[i] = other[i];
   }
@@ -122,6 +128,8 @@ VECGEOM_CUDA_HEADER_BOTH
 VECGEOM_INLINE
 void AOS3D<Type>::Set(const int index, const Type x, const Type y,
                       const Type z) {
+  // assert(index < this->memory_size_);
+  if (index >= this->size_) this->size_ = index+1;
   (data_[index])[0] = x;
   (data_[index])[1] = y;
   (data_[index])[2] = z;
@@ -131,7 +139,29 @@ template <typename Type>
 VECGEOM_CUDA_HEADER_BOTH
 VECGEOM_INLINE
 void AOS3D<Type>::Set(const int index, Vector3D<Type> const &vec) {
+  // assert(index < this->memory_size_);
+  if (index >= this->size_) this->size_ = index+1;
   data_[index] = vec;
+}
+
+template <typename Type>
+VECGEOM_CUDA_HEADER_BOTH
+VECGEOM_INLINE
+void AOS3D<Type>::push_back(const Type x, const Type y, const Type z) {
+  // assert(this->size_ < this->memory_size_);
+  (data_[this->size_])[0] = x;
+  (data_[this->size_])[1] = y;
+  (data_[this->size_])[2] = z;
+  this->size_++;
+}
+
+template <typename Type>
+VECGEOM_CUDA_HEADER_BOTH
+VECGEOM_INLINE
+void AOS3D<Type>::push_back(Vector3D<Type> const &vec) {
+  // assert(this->size_ < this->memory_size_);
+  data_[this->size_] = vec;
+  this->size_++;
 }
 
 #ifdef VECGEOM_CUDA
@@ -140,16 +170,15 @@ template <typename Type> class Vector3D;
 template <typename Type> class AOS3D;
 
 AOS3D<Precision>* AOS3DCopyToGpuInterface(Vector3D<Precision> *const data,
-                                          const unsigned size);
+                                          const int size);
 
 
 template <typename Type>
 AOS3D<Type>* AOS3D<Type>::CopyToGpu(
     Vector3D<Type> *const data_gpu) const {
-  const int count = this->size();
-  const int mem_size = count*sizeof(Vector3D<Type>);
+  const int mem_size = this->size_*sizeof(Vector3D<Type>);
   vecgeom::CopyToGpu(data_, data_gpu, mem_size);
-  return AOS3DCopyToGpuInterface(data_gpu, count);
+  return AOS3DCopyToGpuInterface(data_gpu, this->size_);
 }
 
 #endif // VECGEOM_CUDA
