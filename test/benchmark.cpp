fcf6a534ee91d4e7b4e1f8f034f7e25d61caef9a
Johannes de Fine Licht <johannes.definelicht@cernch>
2014-04-11 10:53:02 +0200

    Change benchmark executable to use three level ROOT geometry.

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -1,47 +1,40 @@
 #include "volumes/logical_volume.h"
 #include "volumes/box.h"
 #include "benchmarking/distance_to_in.h"
+#include "management/rootgeo_manager.h"
+#include "management/geo_manager.h"
+#include "TGeoManager.h"
 
 using namespace vecgeom;
 
+void CreateRootGeometry();
+
 int main() {
 
-  UnplacedBox world_params = UnplacedBox(4., 4., 4.);
-  UnplacedBox largebox_params = UnplacedBox(1.5, 1.5, 1.5);
-  UnplacedBox smallbox_params = UnplacedBox(0.5, 0.5, 0.5);
+  CreateRootGeometry();
+  RootGeoManager::Instance().LoadRootGeometry();
 
-  LogicalVolume world = LogicalVolume(&world_params);
-  VPlacedVolume *world_placed = world.Place();
-  LogicalVolume largebox = LogicalVolume(&largebox_params);
-  LogicalVolume smallbox = LogicalVolume(&smallbox_params);
-
-  TransformationMatrix origin = TransformationMatrix();
-  TransformationMatrix placement1 = TransformationMatrix( 2,  2,  2);
-  TransformationMatrix placement2 = TransformationMatrix(-2,  2,  2);
-  TransformationMatrix placement3 = TransformationMatrix( 2, -2,  2);
-  TransformationMatrix placement4 = TransformationMatrix( 2,  2, -2);
-  TransformationMatrix placement5 = TransformationMatrix(-2, -2,  2);
-  TransformationMatrix placement6 = TransformationMatrix(-2,  2, -2);
-  TransformationMatrix placement7 = TransformationMatrix( 2, -2, -2);
-  TransformationMatrix placement8 = TransformationMatrix(-2, -2, -2);
-
-  largebox.PlaceDaughter(&smallbox, &origin);
-  world.PlaceDaughter(&largebox, &placement1);
-  world.PlaceDaughter(&largebox, &placement2);
-  world.PlaceDaughter(&largebox, &placement3);
-  world.PlaceDaughter(&largebox, &placement4);
-  world.PlaceDaughter(&largebox, &placement5);
-  world.PlaceDaughter(&largebox, &placement6);
-  world.PlaceDaughter(&largebox, &placement7);
-  world.PlaceDaughter(&largebox, &placement8);
-
-  DistanceToInBenchmarker tester(world_placed);
+  DistanceToInBenchmarker tester(GeoManager::Instance().world());
   tester.set_verbose(2);
   tester.set_repetitions(4096);
   tester.set_n_points(1<<13);
   tester.BenchmarkAll();
 
-  delete world_placed;
-
   return 0;
+}
+
+void CreateRootGeometry() {
+  double L = 10.;
+  double Lz = 10.;
+  const double Sqrt2 = sqrt(2.);
+  TGeoVolume * world =  ::gGeoManager->MakeBox("worldl",0, L, L, Lz );
+  TGeoVolume * boxlevel2 = ::gGeoManager->MakeBox("b2l",0, Sqrt2*L/2./2., Sqrt2*L/2./2., Lz );
+  TGeoVolume * boxlevel3 = ::gGeoManager->MakeBox("b3l",0, L/2./2., L/2./2., Lz);
+  TGeoVolume * boxlevel1 = ::gGeoManager->MakeBox("b1l",0, L/2., L/2., Lz );
+  boxlevel2->AddNode( boxlevel3, 0, new TGeoRotation("rot1",0,0,45));
+  boxlevel1->AddNode( boxlevel2, 0, new TGeoRotation("rot2",0,0,-45));
+  world->AddNode(boxlevel1, 0, new TGeoTranslation(-L/2.,0,0));
+  world->AddNode(boxlevel1, 1, new TGeoTranslation(+L/2.,0,0));
+  ::gGeoManager->SetTopVolume(world);
+  ::gGeoManager->CloseGeometry();
 }
