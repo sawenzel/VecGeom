23fd06b922b5624f754908e9bd3f4d12afdda6f2
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2014-04-24 16:48:10 +0200

    VECGEOM-21; simplified version of prototype with suggestions from FishEye.

--- prototype_generation/PlacedVolume.h
+++ prototype_generation/PlacedVolume.h
@@ -7,13 +7,14 @@ class PlacedVolume {
 
 public:
 
-  VECGEOM_CUDA_HEADER_BOTH
+  PlacedVolume() {}
+
+  ~PlacedVolume() {}
+
   virtual bool Inside(const double point[3]) const =0;
 
-#ifdef VECGEOM_VC
   virtual void Inside(const double points[3][VcDouble::Size],
                       bool output[VcDouble::Size]) const =0;
-#endif
 
 };
 
