fe3a3a80cb4f8d07d4e5402e95bebca6bbb2d261
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2014-06-04 17:54:43 +0200

    [VECGEOM-39]: DistToIn method of Paraboloid shape implemented and validated against Root, with both benchmarker and visualization test

--- volumes/kernel/ParaboloidImplementation.h
+++ volumes/kernel/ParaboloidImplementation.h
@@ -49,12 +49,27 @@
 #include "volumes/UnplacedParaboloid.h"
 
 namespace VECGEOM_NAMESPACE {
+    
+    namespace ParaboloidUtilities
+    {
+        template <class Backend>
+        VECGEOM_INLINE
+        VECGEOM_CUDA_HEADER_BOTH
+        void DistToParaboloidSurface(
+                                 UnplacedParaboloid const &unplaced,
+                                 Vector3D<typename Backend::precision_v> const &point,
+                                 Vector3D<typename Backend::precision_v> const &direction,
+                                 typename Backend::precision_v &distance/*,
+                                 typename Backend::bool_v in*/) {
+                                 }
+    }
 
 template <TranslationCode transCodeT, RotationCode rotCodeT>
 struct ParaboloidImplementation {
 
     /// \brief Inside method that takes account of the surface for an Unplaced Paraboloid
     template <class Backend>
+    VECGEOM_INLINE
     VECGEOM_CUDA_HEADER_BOTH
     static void UnplacedInside(UnplacedParaboloid const &unplaced,
                                Vector3D<typename Backend::precision_v> point,
@@ -100,8 +115,9 @@ struct ParaboloidImplementation {
 
     
     /// \brief UnplacedContains (ROOT STYLE): Inside method that does NOT take account of the surface for an Unplaced Paraboloid
-//#if 0
+#if 0
     template <class Backend>
+    VECGEOM_INLINE
     VECGEOM_CUDA_HEADER_BOTH
     static void UnplacedContains(UnplacedParaboloid const &unplaced,
         Vector3D<typename Backend::precision_v> point,
@@ -130,10 +146,11 @@ struct ParaboloidImplementation {
         MaskedAssign(!done, EInside::kInside, &inside);
     }
 
-//#endif
+#endif
     
     /// \brief Inside method that takes account of the surface for a Placed Paraboloid
     template <class Backend>
+    VECGEOM_INLINE
     VECGEOM_CUDA_HEADER_BOTH
     static void Inside(UnplacedParaboloid const &unplaced,
                        Transformation3D const &transformation,
@@ -146,9 +163,10 @@ struct ParaboloidImplementation {
         
     }
     
-//#if 0
+#if 0
     /// \brief Contains: Inside method that does NOT take account of the surface for a Placed Paraboloid
     template <class Backend>
+    VECGEOM_INLINE
     VECGEOM_CUDA_HEADER_BOTH
     static void Contains(UnplacedParaboloid const &unplaced,
                        Transformation3D const &transformation,
@@ -161,9 +179,10 @@ struct ParaboloidImplementation {
         
     }
 
-//#endif
+#endif
     
     template <class Backend>
+    VECGEOM_INLINE
     VECGEOM_CUDA_HEADER_BOTH
     static void DistanceToIn(
                              UnplacedParaboloid const &unplaced,
@@ -172,38 +191,133 @@ struct ParaboloidImplementation {
                              Vector3D<typename Backend::precision_v> const &direction,
                              typename Backend::precision_v const &stepMax,
                              typename Backend::precision_v &distance) {
+        
+        using namespace ParaboloidUtilities;
+        typedef typename Backend::precision_v Float_t;
+        typedef typename Backend::bool_v      Bool_t;
+        
+        
+        Vector3D<Float_t> localPoint =
+        transformation.Transform<transCodeT, rotCodeT>(point);
+        Vector3D<Float_t> localDirection =
+        transformation.TransformDirection<rotCodeT>(direction);
+        
+        Bool_t done(false);
+        distance=kInfinity;
+        
+        Float_t absZ=Abs(localPoint.z()),
+                absDirZ=Abs(localDirection.z()),
+                rho2 = localPoint.x()*localPoint.x()+localPoint.y()*localPoint.y(),
+                point_dot_direction_x = localPoint.x()*localDirection.x(),
+                point_dot_direction_y = localPoint.y()*localDirection.y();
+        
+        Bool_t checkZ=localPoint.z()*localDirection.z() > 0;
+        
+        //check if the point is distancing in Z
+        Bool_t isDistancingInZ= (absZ>unplaced.GetDz() && checkZ);
+        done|=isDistancingInZ;
+        if (done == Backend::kTrue) return;
+        
+        //check if the point is distancing in XY
+        Bool_t isDistancingInXY=( (rho2>unplaced.GetRhi2()) && (point_dot_direction_x>0 || point_dot_direction_y>0) );
+        done|=isDistancingInXY;
+        if (done == Backend::kTrue) return;
     
-        //NYI
+        //is hitting from dz or -dz planes
+        Float_t xHit, yHit, zHit, rhoHit2, distZ, ray2;
+        
+        distZ = (absZ-unplaced.GetDz())/absDirZ;
+        xHit = localPoint.x()+distZ*localDirection.x();
+        yHit = localPoint.y()+distZ*localDirection.y();
+        rhoHit2=xHit*xHit+yHit*yHit;
+        
+        ray2=unplaced.GetRhi2();
+        MaskedAssign(localPoint.z()<-unplaced.GetDz() && localDirection.z()>0, unplaced.GetRlo2(), &ray2); //verificare
+
+        Bool_t isCrossingAtDz= (absZ>unplaced.GetDz()) && (!checkZ) && (rhoHit2 <=ray2);
+        MaskedAssign(isCrossingAtDz, distZ, &distance);
+        done|=isCrossingAtDz;
+        if (done == Backend::kTrue) return;
+        
+        //is hitting from the paraboloid surface
+        //DistToParaboloidSurface<Backend>(unplaced,localPoint,localDirection, distParab);
+        
+        
+        Float_t distParab=kInfinity,
+                dirRho2 = localDirection.x()*localDirection.x()+localDirection.y()*localDirection.y(),
+                a = unplaced.GetA() * dirRho2,
+                b = 2.*unplaced.GetA()*(point_dot_direction_x+point_dot_direction_y)-localDirection.z(),
+                c = unplaced.GetA()*rho2 + unplaced.GetB() - localPoint.z();
+        
+    
+         /*avoiding division per 0
+        Bool_t aVerySmall=(Abs(a)<kTiny),
+               bVerySmall=(Abs(b)<kTiny);
+        
+        done|= aVerySmall && bVerySmall;
+        if (done == Backend::kTrue) return; //big
+        
+        
+        Double_t COverB=-c/b;
+        Bool_t COverBNeg=(COverB<0);
+        done|=COverBNeg && aVerySmall ; //se neg ritorno big
+        if (done == Backend::kTrue) return;
+        
+        Bool_t check1=aVerySmall && !bVerySmall && !COverBNeg;
+        MaskedAssign(!done && check1 , COverB, &distParab ); //da memorizzare dopo
+          */
+        
+        Float_t ainv = 1./a,
+        t = b*0.5,
+        prod = c*a,
+        delta = t*t - prod;
+        
+        Bool_t deltaNeg=delta<0;
+        done|= deltaNeg;
+        if (done == Backend::kTrue) return;
+        
+        //to avoid square root operation on negative elements
+        MaskedAssign(deltaNeg, 0. , &delta);
+        delta = Sqrt(delta);
+        distParab=ainv*(-t - delta);
+        
+        zHit = localPoint.z()+distParab*localDirection.z();
+        Bool_t isHittingParaboloidSurface = ( (distParab > 1E20) || (Abs(zHit)<=unplaced.GetDz()) ); //why: dist > 1E20?
+        MaskedAssign(!done && isHittingParaboloidSurface && !deltaNeg && distParab>0 , distParab, &distance);
+        
     }
+    
 
-
-  template <class Backend>
-  VECGEOM_CUDA_HEADER_BOTH
-  static void DistanceToOut(
+    
+    template <class Backend>
+    VECGEOM_INLINE
+    VECGEOM_CUDA_HEADER_BOTH
+    static void DistanceToOut(
       UnplacedParaboloid const &unplaced,
       Vector3D<typename Backend::precision_v> point,
       Vector3D<typename Backend::precision_v> direction,
       typename Backend::precision_v const &stepMax,
       typename Backend::precision_v &distance) {
-    // NYI
-  }
+      // NYI
+    }
 
-  template<class Backend>
-  VECGEOM_CUDA_HEADER_BOTH
-  static void SafetyToIn(UnplacedParaboloid const &unplaced,
+    template<class Backend>
+    VECGEOM_INLINE
+    VECGEOM_CUDA_HEADER_BOTH
+    static void SafetyToIn(UnplacedParaboloid const &unplaced,
                          Transformation3D const &transformation,
                          Vector3D<typename Backend::precision_v> const &point,
                          typename Backend::precision_v &safety) {
-    // NYI
-  }
-
-  template<class Backend>
-  VECGEOM_CUDA_HEADER_BOTH
-  static void SafetyToOut(UnplacedParaboloid const &unplaced,
+        // NYI
+    }
+    template<class Backend>
+    VECGEOM_INLINE
+    VECGEOM_CUDA_HEADER_BOTH
+    static void SafetyToOut(UnplacedParaboloid const &unplaced,
                           Vector3D<typename Backend::precision_v> point,
                           typename Backend::precision_v &safety) {
-    // NYI
-  }
+        // NYI
+    }
 
 };
 
