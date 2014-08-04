58793da210587a4fe7214aa24618f7ab20ac6155
Tatiana Nikitina <tatiana.nikitina@cern.ch>
2014-08-04 12:09:50 +0200

    [VECGEOM-65]: add input parameter 'vis' for visualisation of results of the test

--- test/shape_tester/shape_testBox.cpp
+++ test/shape_tester/shape_testBox.cpp
@@ -8,22 +8,30 @@
 #ifdef VECGEOM_ROOT
 #include "TApplication.h"
 #endif
+#include "stdlib.h"
 
-int main() {
+int main(  int argc,char *argv[]) {
 
   UBox* box=new UBox("test_box",5.,5.,5.);
   
   ShapeTester tester;
 
-#ifdef VECGEOM_ROOT
-    TApplication theApp("App",0,0);
-#endif
+  if(argc>1)
+  {
+    if(strcmp(argv[1],"vis")==0)
+    {
+     #ifdef VECGEOM_ROOT
+     TApplication theApp("App",0,0);
+     tester.Run(box);
+     theApp.Run();
+     #endif
+    }
+  }
+  else
+  {
+    tester.Run(box);
 
-  tester.Run(box);
-
-#ifdef VECGEOM_ROOT
-  theApp.Run();
-#endif
+   }
 
   return 0;
 }
