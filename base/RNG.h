74663cbec5786ea4620a4fbd304609654e7a1576
Philippe Canal <pcanal@fnal.gov>
2014-08-13 16:11:16 -0500

    [VECGEOM-1] rudimentary random number for cuda compilation
    This is implementation is 'just' to get the compilation to succeed
    and those properly manage (neither did the previous C++11 only implementation)
    the sequence of random number with multiple threads.
    
    Currently this is only used in testing and in GetPointOnSurface, itself
    used for testing.  'The right' solution involves changing the interface
    of GetPointOnSurface to take a random generator object and/or state object.

--- base/RNG.h
+++ base/RNG.h
@@ -6,7 +6,12 @@
 
 #include "base/Global.h"
 
+#ifdef VECGEOM_NVCC
+#include <cuda.h>
+#include <curand_kernel.h>
+#else
 #include <random>
+#endif
 
 namespace VECGEOM_NAMESPACE {
 
@@ -17,30 +22,80 @@ class RNG {
 
 private:
 
+#ifdef VECGEOM_NVCC
+#ifdef __CUDA__ARCH__
+  curandState fState;
+  
+  VECGEOM_CUDA_HEADER_DEVICE
+  VECGEOM_INLINE
+  Precision GetUniform() {
+     return curand_uniform(&fState);
+  }
+#else
+  VECGEOM_CUDA_HEADER_HOST
+  VECGEOM_INLINE
+  Precision GetUniform() {
+     return (Precision) rand() / RAND_MAX;
+  }
+#endif
+
+#else
   std::mt19937 rng;
   std::uniform_real_distribution<> uniform_dist;
 
+  VECGEOM_INLINE
+  Precision GetUniform() {
+     return uniform_dist(rng);
+  }
+
+#endif
+
 protected:
 
+#ifdef VECGEOM_NVCC
+#ifdef __CUDA__ARCH__
+// The state should really be 'thread' specific
+  VECGEOM_CUDA_HEADER_DEVICE
+  RNG() { curand_init(0 /*seed */, 0 /* subsequence */, 0 /* offset */ , &fState); }
+#else
+  RNG() {}
+#endif
+#else
   RNG() : rng(0), uniform_dist(0, 1) {}
+#endif
 
 public:
 
   /**
    * Access singleton instance.
    */
+#ifdef VECGEOM_NVCC
+#ifdef __CUDA__ARCH__
+  VECGEOM_CUDA_HEADER_DEVICE
+  static RNG& Instance() {
+     __shared__ RNG instance;
+#else
+  VECGEOM_CUDA_HEADER_HOST
+  static RNG Instance() {
+     RNG instance;
+#endif
+     return instance;
+  }
+#else
   static RNG& Instance() {
     static RNG instance;
     return instance;
   }
+#endif
 
   /**
    * @return Uniformly distributed floating point number between 0 and 1 unless
    *         range arguments are passed.
    */
+  VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_INLINE
   Precision uniform(const Precision min = 0., const Precision max = 1.) {
-    return min + (max - min) * uniform_dist(rng);
+    return min + (max - min) * GetUniform();
   }
 
 private:
