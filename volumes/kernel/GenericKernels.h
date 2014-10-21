1b37236a0fc4a3f5cfd668c01e420a43878803aa
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2014-10-21 10:12:51 +0200

    [VECGEOM-35]: forced inlining for MakeTolerant-methods and made argument a const reference.

--- volumes/kernel/GenericKernels.h
+++ volumes/kernel/GenericKernels.h
@@ -1,5 +1,5 @@
-/// @file GenericKernels.h
-/// @author Johannes de Fine Licht (johannes.definelicht@cern.ch)
+/// \file GenericKernels.h
+/// \author Johannes de Fine Licht (johannes.definelicht@cern.ch)
 
 #ifndef VECGEOM_VOLUMES_KERNEL_GENERICKERNELS_H_
 #define VECGEOM_VOLUMES_KERNEL_GENERICKERNELS_H_
@@ -20,19 +20,17 @@ struct GenericKernels {
 }; // End struct GenericKernels
 
 template<bool tolerant, typename T>
-T
 VECGEOM_CUDA_HEADER_BOTH
-MakePlusTolerant( T const & x  )
-{
-    return (tolerant)? x+kTolerance : x;
+VECGEOM_INLINE
+T MakePlusTolerant(T const &x) {
+  return (tolerant) ? x+kTolerance : x;
 }
 
 template<bool tolerant, typename T>
-T
 VECGEOM_CUDA_HEADER_BOTH
-MakeMinusTolerant( T x  )
-{
-    return (tolerant)? x-kTolerance : x;
+VECGEOM_INLINE
+T MakeMinusTolerant(T const &x) {
+  return (tolerant) ? x-kTolerance : x;
 }
 
 } // End global namespace
