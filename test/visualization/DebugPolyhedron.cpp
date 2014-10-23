980b935c23ed28ec67119725d88696ed978d66c0
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2014-10-23 17:19:34 +0200

    [VECGEOM-44]: DistanceToOut for phi cutout verified also.

--- test/visualization/DebugPolyhedron.cpp
+++ test/visualization/DebugPolyhedron.cpp
@@ -12,7 +12,7 @@ int main() {
   Precision zPlanes[nPlanes] = {-2, -1, 1, 2};
   Precision rInner[nPlanes] = {1, 1, 1, 1};
   Precision rOuter[nPlanes] = {2, 2, 2, 2};
-  UnplacedPolyhedron unplaced(0, 90, 5, nPlanes, zPlanes, rInner, rOuter);
+  UnplacedPolyhedron unplaced(0, 270, 5, nPlanes, zPlanes, rInner, rOuter);
   LogicalVolume logical(&unplaced);
   Transformation3D translation(0, 0, 0);
   std::unique_ptr<VPlacedVolume> placed(logical.Place(&translation));
@@ -21,7 +21,7 @@ int main() {
   ShapeDebugger debugger(placed.get());
   debugger.SetMaxMismatches(10);
   debugger.ShowCorrectResults(false);
-  debugger.CompareContainsToROOT(bounds, 50000);
+  debugger.CompareDistanceToOutToROOT(bounds, 50000);
 
   return 0;
 }
