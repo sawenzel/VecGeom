e3e6a94a32a8c2a17b82847ebdf54baa67604f01
Philippe Canal <pcanal@fnal.gov>
2014-12-04 13:15:33 -0600

    Remove 'duplicate' boiler plate code

--- volumes/SpecializedBooleanVolume.h
+++ volumes/SpecializedBooleanVolume.h
@@ -10,58 +10,18 @@
 
 namespace vecgeom {
 
-VECGEOM_DEVICE_DECLARE_CONV_TEMPLATE_3v(SpecializedBooleanVolume, BooleanOperation,boolOp, TranslationCode,transCodeT, RotationCode,rotCodeT)
-
 inline namespace VECGEOM_IMPL_NAMESPACE {
 
 template <BooleanOperation boolOp, TranslationCode transCodeT, RotationCode rotCodeT>
-class SpecializedBooleanVolume
-    : public ScalarShapeImplementationHelper<BooleanImplementation<boolOp, transCodeT, rotCodeT> >
-{
+using SpecializedBooleanVolume = ScalarShapeImplementationHelper<BooleanImplementation<boolOp, transCodeT, rotCodeT> >;
 
-  typedef ScalarShapeImplementationHelper<BooleanImplementation<boolOp, transCodeT, rotCodeT> > Helper;
+using GenericPlacedUnionVolume = SpecializedBooleanVolume<kUnion, translation::kGeneric, rotation::kGeneric>;
+using GenericPlacedIntersectionVolume = SpecializedBooleanVolume<kIntersection, translation::kGeneric, rotation::kGeneric>;
+using GenericPlacedSubtractionVolume = SpecializedBooleanVolume<kSubtraction, translation::kGeneric, rotation::kGeneric>;
 
- // typedef TUnplacedBooleanVolume<LeftUnplacedVolume_t, RightPlacedVolume_t> UnplacedVol_t;
- typedef UnplacedBooleanVolume UnplacedVol_t;
-
-public:
-
-#ifndef VECGEOM_NVCC
-
-  SpecializedBooleanVolume(char const *const label,
-                            LogicalVolume const *const logical_volume,
-                            Transformation3D const *const transformation)
-      : Helper(label, logical_volume, transformation, (PlacedBox const *const)nullptr) {}
-
-  SpecializedBooleanVolume(LogicalVolume const *const logical_volume,
-                            Transformation3D const *const transformation)
-      : SpecializedBooleanVolume("", logical_volume, transformation) {}
-
-  virtual ~SpecializedBooleanVolume() {}
-#else
-
-  __device__
-  SpecializedBooleanVolume(LogicalVolume const *const logical_volume,
-                            Transformation3D const *const transformation,
-                            PlacedBox const *const boundingBox, const int id)
-      : Helper(logical_volume, transformation, boundingBox, id) {}
-
-#endif
-
-  VECGEOM_CUDA_HEADER_BOTH
-  virtual void PrintType() const { printf("NOT IMPLEMENTED"); };
-
-}; // endofclassdefinition
-
-typedef SpecializedBooleanVolume<kUnion, translation::kGeneric, rotation::kGeneric> GenericPlacedUnionVolume;
-typedef SpecializedBooleanVolume<kIntersection, translation::kGeneric, rotation::kGeneric> GenericPlacedIntersectionVolume;
-typedef SpecializedBooleanVolume<kSubtraction, translation::kGeneric, rotation::kGeneric> GenericPlacedSubtractionVolume;
-
-typedef SpecializedBooleanVolume<kUnion, translation::kIdentity, rotation::kIdentity> GenericUnionVolume;
-typedef SpecializedBooleanVolume<kIntersection, translation::kIdentity, rotation::kIdentity> GenericIntersectionVolume;
-typedef SpecializedBooleanVolume<kSubtraction, translation::kIdentity, rotation::kIdentity> GenericSubtractionVolume;
-
-
+using GenericUnionVolume = SpecializedBooleanVolume<kUnion, translation::kIdentity, rotation::kIdentity>;
+using GenericIntersectionVolume = SpecializedBooleanVolume<kIntersection, translation::kIdentity, rotation::kIdentity>;
+using GenericSubtractionVolume = SpecializedBooleanVolume<kSubtraction, translation::kIdentity, rotation::kIdentity>;
 
 } // End impl namespace
 
