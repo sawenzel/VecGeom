3eebb7de50518c84c94091fe5d4b2a1126937be2
Guilherme Lima <lima@fnal.gov>
2014-12-17 16:41:11 -0600

    [VECGEOM-35] Fix test/usolids/CompatibilityTest
    The test was failing for the tube, which is not USolids-compatible.
    The trapezoid is now used instead of tube, and the tests pass.

--- test/usolids/CompatibilityTest.cpp
+++ test/usolids/CompatibilityTest.cpp
@@ -1,14 +1,22 @@
 #include "volumes/Box.h"
-#include "volumes/Parallelepiped.h"
-#include "volumes/Tube.h"
+//#include "volumes/Parallelepiped.h"
+//#include "volumes/UTubs.h"
+#include "volumes/Trapezoid.h"
 #include "UBox.hh"
-#include "UTubs.hh"
+#include "UTrap.hh"
 
 using namespace vecgeom;
 
 template <class Box_t>
 VUSolid* ConstructBox(double x, double y, double z) {
   return new Box_t("test", x, y, z);
+}
+
+template <class Tube_t>
+VUSolid* ConstructTrap(double dz, double theta, double phi,
+                       double dy1, double dx1, double dx2, double tanAlpha1,
+                       double dy2, double dx3, double dx4, double tanAlpha2) {
+  return new Tube_t("testTrap", dz, theta, phi, dy1, dx1, dx2, tanAlpha1, dy2, dx3, dx4, tanAlpha2);
 }
 
 template <class Tube_t>
@@ -45,14 +53,36 @@ void CompareTube(VUSolid const *first, VUSolid const *second) {
          < kTolerance);
 }
 
+void CompareTrap(VUSolid const *first, VUSolid const *second) {
+  Vector3D<Precision> normal;
+  bool convex;
+  Vector3D<Precision> insidePoint(1, -1, 0);
+  Vector3D<Precision> outsidePoint(10, -10, -5);
+  Vector3D<Precision> direction(-0.6, 0.6, 0.5291502622129182);
+  assert(first->Inside(insidePoint) == second->Inside(insidePoint));
+  assert(std::abs(first->DistanceToIn(outsidePoint, direction) -
+         second->DistanceToIn(outsidePoint, direction)) < 10.*kTolerance);
+  Assert(std::abs(first->DistanceToOut(insidePoint, direction, normal, convex) -
+         second->DistanceToOut(insidePoint, direction, normal, convex))
+         < kTolerance);
+}
+
 int main() {
   VUSolid *simpleBox = ConstructBox<SimpleBox>(5., 5., 5.);
   VUSolid *uBox = ConstructBox<UBox>(5., 5., 5.);
-  VUSolid *simpleTube = ConstructTube<SimpleTube>(0., 5., 3., 0, kTwoPi);
-  VUSolid *uTube = ConstructTube<UTubs>(0., 5., 3., 0, kTwoPi);
+
+  VUSolid *simpleTrap = ConstructTrap<SimpleTrapezoid>(5, 0.3, 0.4, 4, 2, 3, 0, 8, 4, 6, 0);
+  VUSolid *uTrap = ConstructTrap<UTrap>(5, 0.3, 0.4, 4, 2, 3, 0, 8, 4, 6, 0);
+
   CompareBox(simpleBox, uBox);
+  printf("CompareBox test 1 passed.\n");
   CompareBox(uBox, simpleBox);
-  CompareTube(simpleTube, uTube);
-  CompareTube(uTube, simpleTube);
+  printf("CompareBox test 2 passed.\n");
+
+  CompareTrap(uTrap, simpleTrap);
+  printf("CompareTrap test 1 passed.\n");
+  CompareTrap(simpleTrap, uTrap);
+  printf("CompareTrap test 2 passed.\n");
+
   return 0;
 }
