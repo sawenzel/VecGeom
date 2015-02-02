sawenzel/VecGeom

commit 58d2a1baaf9ef9d0c62be68b58518cd2cc037412

[VecGeom-35] Fix complex_test1, which was failing aos3d an soa3d tests.

base/AOS3D.h
@@ -143,7 +143,7 @@ AOS3D<T>::AOS3D(Vector3D<T> *content, size_t size)
 
 template <typename T>
 AOS3D<T>::AOS3D(size_t sz)
-    : fAllocated(true), fSize(sz), fCapacity(sz), fContent(NULL) {
+    : fAllocated(true), fSize(0), fCapacity(sz), fContent(NULL) {
   reserve(fCapacity);
 }
 

base/SOA3D.h
@@ -178,7 +178,7 @@ SOA3D<T>::SOA3D(T *xval, T *yval, T *zval, size_t sz)
 template <typename T>
 VECGEOM_CUDA_HEADER_BOTH
 SOA3D<T>::SOA3D(size_t sz)
-    : fAllocated(true), fSize(sz), fCapacity(sz),
+    : fAllocated(true), fSize(0), fCapacity(sz),
       fX(NULL), fY(NULL), fZ(NULL) {
   Allocate();
 }

test/root/complex_test1.cpp
@@ -466,13 +466,13 @@ void test_aos3d()
    assert(container1.size() == 0);
    container1.push_back( Vector3D<Precision>(1,0,1));
    assert(container1.size() == 1);
-   std::cerr << "test soa3d passed" << std::endl;
+   std::cerr << "test10: soa3d passed" << std::endl;
 
    AOS3D<Precision> container2(1024);
    assert(container2.size() == 0);
    container2.push_back( Vector3D<Precision>(1,0,1));
    assert(container2.size() == 1);
-   std::cerr << "test aos3d passed" << std::endl;
+   std::cerr << "test10: aos3d passed" << std::endl;
 }
 
 // tests the generation of global points in certain logical reference volumes

commit 7339c3546995e175c59227d901a9c5ae488d93b9

[VecGeom-100] fixes to Polyhedra
Details:
reverting 58d2a1baaf9ef9d0c62be68b58518cd2cc037412 which caused wrong behaviour of
SOA3D and AOS3D classes

fixing construction of bounding tube for polyhedra which was using wrong unit.
TODO: use radias in polyhedra in general just like we do for any other shape

Some problems in Polyhedra are remaining.

base/AOS3D.h
@@ -143,7 +143,7 @@ AOS3D<T>::AOS3D(Vector3D<T> *content, size_t size)
 
 template <typename T>
 AOS3D<T>::AOS3D(size_t sz)
-    : fAllocated(true), fSize(0), fCapacity(sz), fContent(NULL) {
+    : fAllocated(true), fSize(sz), fCapacity(sz), fContent(NULL) {
   reserve(fCapacity);
 }
 

base/SOA3D.h
@@ -178,7 +178,7 @@ SOA3D<T>::SOA3D(T *xval, T *yval, T *zval, size_t sz)
 template <typename T>
 VECGEOM_CUDA_HEADER_BOTH
 SOA3D<T>::SOA3D(size_t sz)
-    : fAllocated(true), fSize(0), fCapacity(sz),
+    : fAllocated(true), fSize(sz), fCapacity(sz),
       fX(NULL), fY(NULL), fZ(NULL) {
   Allocate();
 }
@@ -359,14 +359,17 @@ T const* SOA3D<T>::z() const { return fZ; }
 template <typename T>
 VECGEOM_CUDA_HEADER_BOTH
 void SOA3D<T>::set(size_t index, T xval, T yval, T zval) {
+  assert(index < fCapacity);
   fX[index] = xval;
   fY[index] = yval;
   fZ[index] = zval;
+
 }
 
 template <typename T>
 VECGEOM_CUDA_HEADER_BOTH
 void SOA3D<T>::set(size_t index, Vector3D<T> const &vec) {
+  assert(index < fCapacity);
   fX[index] = vec[0];
   fY[index] = vec[1];
   fZ[index] = vec[2];

source/UnplacedPolyhedron.cpp
@@ -37,7 +37,7 @@ UnplacedPolyhedron::UnplacedPolyhedron(
       fPhiStart(phiStart),fPhiDelta(phiDelta),
       fZSegments(zPlaneCount-1), fZPlanes(zPlaneCount), fRMin(zPlaneCount),
       fRMax(zPlaneCount), fPhiSections(sideCount+1),
-      fBoundingTube(0, 1, 1, 0, 360) {
+      fBoundingTube(0, 1, 1, 0, kTwoPi) {
 
   typedef Vector3D<Precision> Vec_t;
 
@@ -105,7 +105,7 @@ UnplacedPolyhedron::UnplacedPolyhedron(
   Precision boundsPhiDelta = !fHasPhiCutout ? 360 : phiDelta;
   fBoundingTube = UnplacedTube(innerRadius - kTolerance,
                                outerRadius + kTolerance, 0.5*boundingTubeZ,
-                               boundsPhiStart, boundsPhiDelta);
+                               boundsPhiStart*kDegToRad, boundsPhiDelta*kDegToRad);
   fBoundingTubeOffset = zPlanes[0] + 0.5*boundingTubeZ;
 
   // Ease indexing into twodimensional vertix array

volumes/Quadrilaterals.h
@@ -53,6 +53,7 @@ public:
   VECGEOM_CUDA_HEADER_BOTH
   Quadrilaterals& operator=(Quadrilaterals const &other);
 
+  // returns the number of quadrilaterals ( planes ) stored in this container
   VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_INLINE
   int size() const;

volumes/kernel/PolyhedronImplementation.h
@@ -355,7 +355,7 @@ int FindZSegmentKernel<kScalar>(
     Precision const *end,
     Precision const &pointZ) {
   // TODO: vectorize this and move the brute-force algorithm to the CUDA
-  //       implemetation. Inspiration can be found at:
+  //       implementation. Inspiration can be found at:
   //       http://schani.wordpress.com/2010/04/30/linear-vs-binary-search/
   int index = -1;
   while (begin < end && pointZ > *begin) {
@@ -697,6 +697,7 @@ bool PolyhedronImplementation<innerRadiiT, phiCutoutT>::ScalarContainsKernel(
 
   // Find correct segment by checking Z-bounds
   int zIndex = FindZSegment<kScalar>(polyhedron, localPoint[2]);
+  if( ! (zIndex >= 0) ) return false;
 
   ZSegment const &segment = polyhedron.GetZSegment(zIndex);
 