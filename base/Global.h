5cbe120acb893e1f821258b533aa4617d26dda6c
Laurent Duhem <laurent.duhem@intel.com>
2015-02-04 14:07:13 +0100

    Fix 64Bytes alignment for Xeon PHI

--- base/Global.h
+++ base/Global.h
@@ -244,7 +244,11 @@ inline namespace VECGEOM_IMPL_NAMESPACE {
    };
 #endif
 
+#if defined (__MIC__)
+VECGEOM_GLOBAL int kAlignmentBoundary = 64;
+#else
 VECGEOM_GLOBAL int kAlignmentBoundary = 32;
+#endif
 VECGEOM_GLOBAL Precision kPi = 3.14159265358979323846;
 VECGEOM_GLOBAL Precision kTwoPi = 2.*kPi;
 VECGEOM_GLOBAL Precision kTwoPiInv = 1./kTwoPi;
