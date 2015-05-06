73ec99d3974d5244cb8bc4528bbf69a9173f426b
Sandro Wenzel <sandro.wenzel@cern.ch>
2015-05-06 08:44:19 +0200

    moving a function from header to source

--- navigation/ABBoxNavigator.h
+++ navigation/ABBoxNavigator.h
@@ -383,62 +383,6 @@ Precision ABBoxNavigator::GetSafety(Vector3D<Precision> const & globalpoint,
    return safety;
 }
 
-inline
- int ABBoxNavigator::GetHitCandidates(
-                         LogicalVolume const * lvol,
-                         Vector3D<Precision> const & point,
-                         Vector3D<Precision> const & dir,
-                         ABBoxManager::ABBoxContainer_t const & corners, int size,
-                         ABBoxManager::HitContainer_t & hitlist) const {
-
-    Vector3D<Precision> invdir(1./dir.x(), 1./dir.y(), 1./dir.z());
-    int vecsize = size;
-    int hitcount = 0;
-    int sign[3]; sign[0] = invdir.x() < 0; sign[1] = invdir.y() < 0; sign[2] = invdir.z() < 0;
-    // interpret as binary number and do a switch statement
-    // do a big switch statement here
-   // int code = 2 << size[0] + 2 << size[1] + 2 << size[2];
-    for( auto box = 0; box < vecsize; ++box ){
-         double distance = BoxImplementation<translation::kIdentity, rotation::kIdentity>::IntersectCachedKernel2<kScalar, double>(
-            &corners[2*box],
-            point,
-           invdir,
-           sign[0],sign[1],sign[2],
-            0, vecgeom::kInfinity );
-            if( distance < vecgeom::kInfinity ){
-                hitcount++;
-             hitlist.push_back( ABBoxManager::BoxIdDistancePair_t( box, distance) );
-            }
-        }
-
-    //    switch( size[0] + size[1] + size[2] ){
-//    case 0: {
-//        for( auto box = 0; box < vecsize; ++box ){
-//        double distance = BoxImplementation<translation::kIdentity, rotation::kIdentity>::IntersectCachedKernel<kScalar,0,0,0>(
-//           &corners[2*box],
-//           point,
-//           invdir,
-//           0, vecgeom::kInfinity );
-//           if( distance < vecgeom::kInfinity ) hitcount++;
-//         }       break; }
-//    case 3: {
-//        for( auto box = 0; box < vecsize; ++box ){
-//                double distance = BoxImplementation<translation::kIdentity, rotation::kIdentity>::IntersectCachedKernel<kScalar,1,1,1>(
-//                   &corners[2*box],
-//                   point,
-//                   invdir,
-//                   0, vecgeom::kInfinity );
-//                   if( distance < vecgeom::kInfinity ) hitcount++;
-//                 }       break; }
-//    default : std::cerr << "DEFAULT CALLED\n";
-//    }
-#ifdef INNERTIMER
-    timer.Stop();
-    std::cerr << "# CACHED hitting " << hitcount << "\n";
-    std::cerr << "# CACHED timer " << timer.Elapsed() << "\n";
-#endif
-    return hitcount;
-}
 } } // End global namespace
 
 #endif /* ABBOXNAVIGATOR_H_ */
