94c525082bbba45fd46f5820066475c32af800a1
Philippe Canal <pcanal@fnal.gov>
2015-05-15 14:10:28 -0500

    VecGeom-118]: add missing cast

--- source/PlacedBooleanVolume.cpp
+++ source/PlacedBooleanVolume.cpp
@@ -33,13 +33,16 @@ Vector3D<Precision> PlacedBooleanVolume::GetPointOnSurface() const {
            return p;
        }
 
+       
+       UnplacedBooleanVolume *unplaced = (UnplacedBooleanVolume*)GetUnplacedVolume();
        if( RNG::Instance().uniform() < arearatio ){
-           p = GetUnplacedVolume()->fLeftVolume->GetPointOnSurface();
+          p = ((UnplacedBooleanVolume *)unplaced->fLeftVolume)->GetPointOnSurface();
        }
        else {
-           p = GetUnplacedVolume()->fRightVolume->GetPointOnSurface();
+          p = ((UnplacedBooleanVolume *)unplaced->fRightVolume)->GetPointOnSurface();
        }
     } while( Inside(p) != vecgeom::kSurface );
+    return p;
 }
 
 #ifdef VECGEOM_NVCC
