5875127a055408ee326a59389d4b5e724976eb18
Sandro Wenzel <sandro.wenzel@cern.ch>
2015-07-14 15:50:31 +0200

    commenting out duplicate Pow in scalar backend; fixes compilation error

--- backend/scalar/Backend.h
+++ backend/scalar/Backend.h
@@ -87,12 +87,12 @@ bool IsEmpty(bool const &cond){
     return !cond;
 }
 
-template <typename Type>
-VECGEOM_CUDA_HEADER_BOTH
-VECGEOM_INLINE
-Type Pow(Type const &x, Type arg) {
-   return pow(x,arg);
-}
+/* template <typename Type> */
+/* VECGEOM_CUDA_HEADER_BOTH */
+/* VECGEOM_INLINE */
+/* Type Pow(Type const &x, Type arg) { */
+/*    return pow(x,arg); */
+/* } */
 
 template <typename Type>
 VECGEOM_CUDA_HEADER_BOTH
