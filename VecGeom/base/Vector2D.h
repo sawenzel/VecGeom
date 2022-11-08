20264fae335fcc1d2f1946240f85b4838399d849
Andrei Gheata <Andrei.Gheata@cern.ch>
2022-11-08 14:56:50 +0100

    [surface model] Added new functionality needed by the surface model to Vector2D

--- VecGeom/base/Vector2D.h
+++ VecGeom/base/Vector2D.h
@@ -35,6 +35,9 @@ public:
   Vector2D(const Type x, const Type y);
 
   VECCORE_ATT_HOST_DEVICE
+  Vector2D(const Type x);
+
+  VECCORE_ATT_HOST_DEVICE
   Vector2D(Vector2D const &other);
 
   VECCORE_ATT_HOST_DEVICE
@@ -70,7 +73,86 @@ public:
 
   VECCORE_ATT_HOST_DEVICE
   VECGEOM_FORCE_INLINE
-  Type Cross(VecType const &other) const { return vec[0] * other.vec[1] - vec[1] * other.vec[0]; }
+  void Set(const Type a);
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Vector2D<Type> Abs() const;
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Min() const;
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Max() const;
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Vector2D<Type> Unit() const;
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type CrossZ(VecType const &other) const;
+
+  /// The dot product of two Vector2D<T> objects
+  /// \return T (where T is float, double, or various SIMD vector types)
+  template <typename Type2>
+  VECGEOM_FORCE_INLINE
+  VECCORE_ATT_HOST_DEVICE
+  static Type Dot(Vector2D<Type> const &left, Vector2D<Type2> const &right)
+  {
+    return left[0] * right[0] + left[1] * right[1];
+  }
+
+  /// The dot product of two Vector2D<T> objects
+  /// \return T (where T is float, double, or various SIMD vector types)
+  template <typename Type2>
+  VECGEOM_FORCE_INLINE
+  VECCORE_ATT_HOST_DEVICE
+  Type Dot(Vector2D<Type2> const &right) const
+  {
+    return Dot(*this, right);
+  }
+
+  /// \return Squared magnitude of the vector.
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Mag2() const;
+
+  /// \return Magnitude of the vector.
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Mag() const;
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Length() const;
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Length2() const;
+
+  /// Normalizes the vector by dividing each entry by the length.
+  /// \sa Vector2D::Length()
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  void Normalize();
+
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Vector2D<Type> Normalized() const;
+
+  // checks if vector is normalized
+  // only reasonable to call with standard scalare usage
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  bool IsNormalized() const;
+
+  /// \return Azimuthal angle between -pi and pi.
+  VECCORE_ATT_HOST_DEVICE
+  VECGEOM_FORCE_INLINE
+  Type Phi() const;
 
 #define VECTOR2D_TEMPLATE_INPLACE_BINARY_OP(OPERATOR) \
   VECCORE_ATT_HOST_DEVICE                             \
@@ -110,6 +192,14 @@ Vector2D<Type>::Vector2D(const Type x, const Type y)
 {
   vec[0] = x;
   vec[1] = y;
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Vector2D<Type>::Vector2D(const Type x)
+{
+  vec[0] = x;
+  vec[1] = x;
 }
 
 template <typename Type>
@@ -177,6 +267,109 @@ void Vector2D<Type>::Set(const Type x, const Type y)
 {
   vec[0] = x;
   vec[1] = y;
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+void Vector2D<Type>::Set(const Type a)
+{
+  Set(a, a);
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Vector2D<Type> Vector2D<Type>::Abs() const
+{
+  return Vector2D<Type>(vecCore::math::Abs(vec[0]), vecCore::math::Abs(vec[1]));
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Min() const
+{
+  return vecCore::math::Min(vec[0], vec[1]);
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Max() const
+{
+  return vecCore::math::Max(vec[0], vec[1]);
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Vector2D<Type> Vector2D<Type>::Unit() const
+{
+  const Type mag2 = Mag2();
+  Vector2D<Type> output(*this);
+  output /= Sqrt(mag2 + kMinimum);
+  return output;
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::CrossZ(VecType const &other) const
+{
+  return vec[0] * other.vec[1] - vec[1] * other.vec[0];
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Mag2() const
+{
+  return Dot(*this, *this);
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Mag() const
+{
+  return Sqrt(Mag2());
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Length() const
+{
+  return Mag();
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Length2() const
+{
+  return Mag2();
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+void Vector2D<Type>::Normalize()
+{
+  *this *= (Type(1.) / Length());
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Vector2D<Type> Vector2D<Type>::Normalized() const
+{
+  return Vector2D<Type>(*this) * (Type(1.) / Length());
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+bool Vector2D<Type>::IsNormalized() const
+{
+  // static_assert here that Type should be primitive type
+  Precision norm = Mag2();
+  return Type(1.) - vecgeom::kTolerance < norm && norm < Type(1.) + vecgeom::kTolerance;
+}
+
+template <typename Type>
+VECCORE_ATT_HOST_DEVICE
+Type Vector2D<Type>::Phi() const
+{
+  return ATan2(vec[1], vec[0]);
 }
 
 template <typename Type>
