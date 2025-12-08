94fd98664bd44cb0978427935ff29c40184f9b39
Seth Johnson <sethrj.ornl@icloud.com>
2025-12-08 10:42:33 +0000

    Move device globals to device global file to fix IWYU errors
    Several device data are defined in CudaManager, which isn't included in NavStateIndex/Tuple but which require the global data. (The previous RC candidate didn't include the CudaManager so I was getting build errors.)
    
    * Added missing forward declarations.
    
    * Move device globals to device global file

--- VecGeom/management/DeviceGlobals.h
+++ VecGeom/management/DeviceGlobals.h
@@ -4,16 +4,31 @@
 #ifndef VECGEOM_MANAGEMENT_DEVICEGLOBALS_H_
 #define VECGEOM_MANAGEMENT_DEVICEGLOBALS_H_
 
+#include "VecGeom/base/Cuda.h"
+#include "VecGeom/base/Global.h"
+
+#pragma once
 namespace vecgeom {
 
+namespace cuda {
+class VPlacedVolume;
+class LogicalVolume;
+} // namespace cuda
+
 VECGEOM_DEVICE_FORWARD_DECLARE(struct VolumeTree;);
+VECGEOM_DEVICE_FORWARD_DECLARE(class LogicalVolume;);
+VECGEOM_DEVICE_FORWARD_DECLARE(class VPlacedVolume;);
 
 struct VolumeTree;
 
 // we put some global data into a separate namespace
 // this is done since CUDA does not support static const members in class definitions
 namespace globaldevicegeomdata {
-inline VECCORE_ATT_DEVICE VolumeTree *gVolumeTree = nullptr;
+inline VECCORE_ATT_DEVICE VolumeTree *gVolumeTree                      = nullptr;
+inline VECCORE_ATT_DEVICE cuda::VPlacedVolume *gCompactPlacedVolBuffer = nullptr;
+inline VECCORE_ATT_DEVICE cuda::LogicalVolume *gDeviceLogicalVolumes   = nullptr;
+inline VECCORE_ATT_DEVICE NavIndex_t *gNavIndex                        = nullptr; // address of navigation index table
+inline VECCORE_ATT_DEVICE int gMaxDepth                                = 0;
 } // namespace globaldevicegeomdata
 } // namespace vecgeom
 #endif
