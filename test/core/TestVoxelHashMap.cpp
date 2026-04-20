3d68b3df009c50d8fc22555d029ebfb8e79d0bcf
Benjamin Morgan <ben.morgan@warwick.ac.uk>
2026-04-20 17:30:44 +0200

    Import and demonstrate GoogleTest use for VecGeom
    Per discussion today, this implements the minimum infrastructure for using GoogleTest in VecGeom:
    
    1. Uses `find_package` to locate GoogleTest.
    2. Falls back to get it via `FetchContent` if not available on the host.
    3. Implements a CMake `vecgeom_add_test` function to wrap the basic build/test of a source file
    4. Converts the `core/TestVector.cpp` to GoogleTest style.
    
    This is very minimal!
    
    * Move GTest setup to top level CMake script.
    
    Keeps setup related to BUILD_TESTING and use of GTest global in case
    of adding further subdirectories.
    
    * Convert TestVoxelHashMap to GoogleTest
    
    * Use clearer EXPECT_EQ macro
    
    * Demo use of GoogleTest for VecGeom
    
    MRE for implementing a CPU-only VecGeom test using GoogleTest. Ideas
    largely taken from Celeritas/Geant4 unit tests.

--- test/core/TestVoxelHashMap.cpp
+++ test/core/TestVoxelHashMap.cpp
@@ -1,84 +1,68 @@
 #include "VecGeom/base/FlatVoxelHashMap.h"
 #include "VecGeom/base/Vector3D.h"
-#undef NDEBUG
-#include "VecGeom/base/Assert.h"
+
+#include <gtest/gtest.h>
 
 using namespace vecgeom;
 
-void testGeneralVersion()
-{
-  int Nx = 10;
-  int Ny = 10;
-  int Nz = 10;
-  Vector3D<float> lower(-5, -5, -5);
-  Vector3D<float> dim(10, 10, 10);
+class VoxelHashMapTest : public ::testing::Test {
+protected:
+  static constexpr int Nx = 10, Ny = 10, Nz = 10;
+  const Vector3D<float> lower{-5, -5, -5};
+  const Vector3D<float> dim{10, 10, 10};
+  const Vector3D<float> p1{-4.9f, -4.9f, -4.9f};
+  const Vector3D<float> p2{-4.85f, -4.85f, -4.85f};
+  const Vector3D<float> p3{4.99f, 4.99f, 4.99f};
+};
 
+TEST_F(VoxelHashMapTest, GeneralVersion)
+{
   // a voxel structure mapping to vector of ints per voxel
   FlatVoxelHashMap<int> voxels(lower, dim, Nx, Ny, Nz);
 
-  Vector3D<float> p1(-4.9f, -4.9f, -4.9f);
-  VECGEOM_ASSERT(voxels.getVoxelKey(p1) == 0);
-  Vector3D<float> p3(4.99f, 4.99f, 4.99f);
-  VECGEOM_ASSERT(voxels.getVoxelKey(p3) == Nx * Ny * Nz - 1);
+  EXPECT_EQ(voxels.getVoxelKey(p1), 0);
+  EXPECT_EQ(voxels.getVoxelKey(p3), Nx * Ny * Nz - 1);
 
   int length{0};
-  VECGEOM_ASSERT(voxels.isOccupied(p1) == false);
-  VECGEOM_ASSERT(voxels.getProperties(p1, length) == nullptr);
-  VECGEOM_ASSERT(length == 0);
+  EXPECT_FALSE(voxels.isOccupied(p1));
+  EXPECT_EQ(voxels.getProperties(p1, length), nullptr);
+  EXPECT_EQ(length, 0);
 
   voxels.addProperty(p1, 111);
   voxels.addProperty(p1, 112);
-  VECGEOM_ASSERT(voxels.isOccupied(p1) == true);
-  VECGEOM_ASSERT(voxels.getProperties(p1, length) != nullptr);
-  VECGEOM_ASSERT(length == 2);
+  EXPECT_TRUE(voxels.isOccupied(p1));
+  EXPECT_NE(voxels.getProperties(p1, length), nullptr);
+  EXPECT_EQ(length, 2);
   auto props = voxels.getProperties(p1, length);
-  VECGEOM_ASSERT(props[0] == 111);
-  VECGEOM_ASSERT(props[1] == 112);
+  EXPECT_EQ(props[0], 111);
+  EXPECT_EQ(props[1], 112);
 
   // nearby point in same voxel
-  Vector3D<float> p2(-4.85f, -4.85f, -4.85f);
-  VECGEOM_ASSERT(voxels.isOccupied(p2) == true);
-  VECGEOM_ASSERT(voxels.getProperties(p2, length) != nullptr);
+  EXPECT_TRUE(voxels.isOccupied(p2));
+  EXPECT_NE(voxels.getProperties(p2, length), nullptr);
 }
 
-void testScalarVersion()
+TEST_F(VoxelHashMapTest, ScalarVersion)
 {
-  int Nx = 10;
-  int Ny = 10;
-  int Nz = 10;
-  Vector3D<float> lower(-5, -5, -5);
-  Vector3D<float> dim(10, 10, 10);
-
   // a voxel structure mapping to a single int per voxel
   FlatVoxelHashMap<int, true> voxels(lower, dim, Nx, Ny, Nz);
 
-  Vector3D<float> p1(-4.9f, -4.9f, -4.9f);
-  VECGEOM_ASSERT(voxels.getVoxelKey(p1) == 0);
-  Vector3D<float> p3(4.99f, 4.99f, 4.99f);
-  VECGEOM_ASSERT(voxels.getVoxelKey(p3) == Nx * Ny * Nz - 1);
+  EXPECT_EQ(voxels.getVoxelKey(p1), 0);
+  EXPECT_EQ(voxels.getVoxelKey(p3), Nx * Ny * Nz - 1);
 
   int length{0};
-  VECGEOM_ASSERT(voxels.isOccupied(p1) == false);
-  VECGEOM_ASSERT(voxels.getProperties(p1, length) == nullptr);
-  VECGEOM_ASSERT(length == 0);
+  EXPECT_FALSE(voxels.isOccupied(p1));
+  EXPECT_EQ(voxels.getProperties(p1, length), nullptr);
+  EXPECT_EQ(length, 0);
 
   voxels.addProperty(p1, 111);
-  VECGEOM_ASSERT(voxels.isOccupied(p1) == true);
-  VECGEOM_ASSERT(voxels.getProperties(p1, length) != nullptr);
-  VECGEOM_ASSERT(length == 1);
+  EXPECT_TRUE(voxels.isOccupied(p1));
+  EXPECT_NE(voxels.getProperties(p1, length), nullptr);
+  EXPECT_EQ(length, 1);
   auto props = voxels.getProperties(p1, length);
-  VECGEOM_ASSERT(props[0] == 111);
+  EXPECT_EQ(props[0], 111);
 
   // nearby point in same voxel
-  Vector3D<float> p2(-4.85f, -4.85f, -4.85f);
-  VECGEOM_ASSERT(voxels.isOccupied(p2) == true);
-  VECGEOM_ASSERT(voxels.getProperties(p2, length) != nullptr);
-}
-
-int main()
-{
-  testGeneralVersion();
-  testScalarVersion();
-  std::cout << "test passed\n";
-  return 0;
+  EXPECT_TRUE(voxels.isOccupied(p2));
+  EXPECT_NE(voxels.getProperties(p2, length), nullptr);
 }
